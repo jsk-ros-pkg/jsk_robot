d8f02ff210afba49c54416e2813a1076a76c69f0
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2012-04-06 08:46:10 +0000

    fix check_openni_node launch

--- jsk_pr2_startup/jsk_pr2_sensors/check_openni_node.py
+++ jsk_pr2_startup/jsk_pr2_sensors/check_openni_node.py
@@ -13,6 +13,7 @@ class image_converter:
 
     def __init__(self):
         self.bridge = CvBridge()
+        self.image_sub = None
         
     def callback(self,data):
         self.image_sub.unregister()
@@ -22,7 +23,7 @@ class image_converter:
         except CvBridgeError, e:
             print e
 
-        rospy.loginfo("sum of pixels %d at %s.%s", max(cv.Sum(cv_image)), data.header.stamp.secs, data.header.stamp.nsecs)
+        rospy.loginfo("sum of pixels is %d at %s", max(cv.Sum(cv_image)), data.header.stamp.secs)
         if max(cv.Sum(cv_image)) == 0 :
             rospy.logerr("Restart openni_node1")
             retcode = -1
@@ -32,7 +33,8 @@ class image_converter:
                 rospy.logerr('Unable to kill kinect node, caught exception:\n%s', traceback.format_exc())
 
     def process(self):
-        self.image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,self.callback)
+        if self.image_sub == None or self.image_sub.impl == None:
+            self.image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,self.callback,None,1)
 
 
 def main(args):
@@ -40,7 +42,7 @@ def main(args):
     rospy.init_node('check_openni_node', anonymous=True)
     while not rospy.is_shutdown():
         ic.process()
-        rospy.sleep(1)
+        rospy.sleep(3)
           
 
 if __name__ == '__main__':
