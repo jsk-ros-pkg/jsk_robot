8382c517aa57a13d5861d7c7ed9b69bf1aa2be5f
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2012-08-08 01:48:41 +0000

    update status with media if dat.data contains image file path

--- jsk_pr2_startup/jsk_pr2_lifelog/tweet.py
+++ jsk_pr2_startup/jsk_pr2_lifelog/tweet.py
@@ -4,6 +4,7 @@ import roslib
 roslib.load_manifest('jsk_pr2_startup')
 import rospy
 import twoauth,yaml
+import re
 from std_msgs.msg import String
 
 # see http://d.hatena.ne.jp/gumilab/20101004/1286154912 to setup CKEY/AKEY
@@ -14,8 +15,16 @@ AKEY = key['AKEY']
 ASECRET = key['ASECRET']
 
 def twit(dat):
-    rospy.loginfo(rospy.get_name()+" sending %s",dat.data)
-    twitter.status_update(dat.data)
+    message = dat.data
+    rospy.loginfo(rospy.get_name()+" sending %s",message)
+    # search word start from / and end with {.jpeg,.jpg,.png,.gif}
+    m = re.search('/\S+\.(jpeg|jpg|png|gif)', message)
+    if m :
+        filename = m.group(0)
+        message = re.sub(filename,"",message)
+        twitter.status_update_with_media(message, filename)
+    else:
+        twitter.status_update(message)
 
 if __name__ == '__main__':
     twitter = twoauth.api(CKEY, CSECRET, AKEY, ASECRET)
