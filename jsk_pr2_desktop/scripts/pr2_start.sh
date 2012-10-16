1ee08f4057fc1dce4d58ea8f6e08b882c9c67d41
youhei <youhei@jsk.imi.i.u-tokyo.ac.jp>
2012-10-16 15:05:49 +0000

    add y/n query before robot stop

--- jsk_pr2_desktop/scripts/pr2_start.sh
+++ jsk_pr2_desktop/scripts/pr2_start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set +e
 
+AUSER=$(ssh applications@pr1012 "(PS1=applications source .bashrc &&  robot users --no-plist | grep 'Active User' | cut -d ' '  -f 3)")
+
+if [ ! "${AUSER}" == "None"  ]; then
+    read -p "${AUSER} is claiming PR2 !!! Would you like to use PR2? (y/n): "
+    [ "$REPLY" == "y" -o  "$REPLY" == "Y" -o "$REPLY" == "yes" -o "$REPLY" == "YES" ] || exit 1;
+fi
+
 yes | ssh applications@pr1012 "(PS1=applications source .bashrc && robot claim)"
 yes | ssh applications@pr1012 "(PS1=applications source .bashrc && robot stop)"
 sleep 2
--- jsk_pr2_desktop/scripts/pr2_stop.sh
+++ jsk_pr2_desktop/scripts/pr2_stop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set +e
 
+AUSER=$(ssh applications@pr1012 "(PS1=applications source .bashrc &&  robot users --no-plist | grep 'Active User' | cut -d ' '  -f 3)")
+if [ ! "${AUSER}" == "None"  ]; then
+    read -p "${AUSER} is claiming PR2! Would you like to stop PR2? (y/n): "
+    [ "$REPLY" == "y" -o  "$REPLY" == "Y" -o "$REPLY" == "yes" -o "$REPLY" == "YES" ] || exit 1;
+fi
+
 yes | ssh applications@pr1012 "(PS1=applications source .bashrc && robot stop)"
 yes | ssh applications@pr1012 "(PS1=applications source .bashrc && robot release)"
 # pkill -9 rviz
