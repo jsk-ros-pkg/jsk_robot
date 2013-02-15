3dc4f4c86253cc22ed28f8d74dec4198a7341628
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-02-15 06:43:11 +0000

    add rossetip and rossetrobot

--- jsk_pr2_desktop/scripts/pr2_jsk_startup.sh
+++ jsk_pr2_desktop/scripts/pr2_jsk_startup.sh
@@ -12,7 +12,7 @@ export ROS_MASTER_URI=http://${PR2_NAME}:11311
 ## for desktop
 sleep 1
 rosrun pr2_dashboard pr2_dashboard &
-xterm -sb -rightbar -sl 99999 +s -title "RVIZ" -e "export PS1=pr2admin; source ~/.bashrc; ROS_MASTER_URI=http://${PR2_NAME}:11311 rosrun rviz rviz -d $(rospack find jsk_pr2_startup)/config/jsk_startup.vcg" &
+xterm -sb -rightbar -sl 99999 +s -title "RVIZ" -e "export PS1=pr2admin; source ~/.bashrc; rossetip; rossetrobot ${PR2_NAME}; ROS_MASTER_URI=http://${PR2_NAME}:11311 rosrun rviz rviz -d $(rospack find jsk_pr2_startup)/config/jsk_startup.vcg" &
 
 ## for robot
 sleep 1
