c1df2d218a701d3e3b3f54ec35e9f1779b2ea9a2
youhei <youhei@jsk.imi.i.u-tokyo.ac.jp>
2013-11-05 16:51:37 +0000

    fix loading parameters and update

--- jsk_pr2_startup/jsk_pr2_sensors/publish_empty_cloud.py
+++ jsk_pr2_startup/jsk_pr2_sensors/publish_empty_cloud.py
@@ -8,44 +8,50 @@ from sensor_msgs.msg import *
 def talker():
     rospy.init_node('empty_cloud_publisher')
     pub = rospy.Publisher('empty_cloud', PointCloud2)
-    frame_id = rospy.get_param('frame_id', '/base_laser_link')
+    frame_id = rospy.get_param('~frame_id', '/base_laser_link')
+    max_range = rospy.get_param('~max_range', 25.0)
+
+    irange = range(-314, 315)
+
+    msg = PointCloud2()
+    msg.header.frame_id = frame_id
+    msg.height = 1
+    msg.width = len(irange)
+
+    msg_fieldx = PointField()
+    msg_fieldy = PointField()
+    msg_fieldz = PointField()
+    msg_fieldx.name = 'x'
+    msg_fieldx.offset = 0
+    msg_fieldx.datatype = 7
+    msg_fieldx.count = 1
+    msg_fieldy.name = 'y'
+    msg_fieldy.offset = 4
+    msg_fieldy.datatype = 7
+    msg_fieldy.count = 1
+    msg_fieldz.name = 'z'
+    msg_fieldz.offset = 8
+    msg_fieldz.datatype = 7
+    msg_fieldz.count = 1
+
+    msg.fields = [msg_fieldx, msg_fieldy, msg_fieldz]
+    msg.point_step = 16
+    msg.row_step = msg.point_step * msg.width;
+    msg.is_dense = True
+
+    for i in irange:
+        x = max_range * math.cos(i*0.005)
+        y = max_range * math.sin(i*0.005)
+        z = 0.0
+        msg.data += pack('<f', x)
+        msg.data += pack('<f', y)
+        msg.data += pack('<f', z)
+        msg.data += pack('<f', 0.0)
+
+
     while not rospy.is_shutdown():
-        msg = PointCloud2()
+
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = frame_id
-        msg.height = 1
-        msg.width = 72
-
-
-        msg_fieldx = PointField()
-        msg_fieldy = PointField()
-        msg_fieldz = PointField()
-        msg_fieldx.name = 'x'
-        msg_fieldx.offset = 0
-        msg_fieldx.datatype = 7
-        msg_fieldx.count = 1
-        msg_fieldy.name = 'y'
-        msg_fieldy.offset = 4
-        msg_fieldy.datatype = 7
-        msg_fieldy.count = 1
-        msg_fieldz.name = 'z'
-        msg_fieldz.offset = 8
-        msg_fieldz.datatype = 7
-        msg_fieldz.count = 1
-
-        msg.fields = [msg_fieldx, msg_fieldy, msg_fieldz]
-        msg.point_step = 16
-        msg.row_step = msg.point_step * msg.width;
-        msg.is_dense = True
-
-        for i in range(-32,32):
-            x = 25*math.cos(i*0.01)
-            y = 25*math.sin(i*0.01)
-            z = 0.0
-            msg.data += pack('<f', x)
-            msg.data += pack('<f', y)
-            msg.data += pack('<f', z)
-            msg.data += pack('<f', 0)
 
         pub.publish(msg)
         rospy.sleep(0.1)
