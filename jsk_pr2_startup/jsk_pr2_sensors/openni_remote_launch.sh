29672e1044fc2b1f03e30ba26db5bc3bcf169235
tarukosu <u_au_sky_future12@yahoo.co.jp>
2014-07-02 01:05:40 +0900

    remap tf for remote

--- jsk_pr2_startup/jsk_pr2_sensors/openni_remote_launch.sh
+++ jsk_pr2_startup/jsk_pr2_sensors/openni_remote_launch.sh
@@ -3,4 +3,4 @@ set +e
 MACHINE_NAME=$1
 if [ "$1" = "" ]; then MACHINE_NAME="c2"; fi
 
-ssh -t -l applications ${MACHINE_NAME} "export PS1=applications;source ~/.bashrc; roslaunch jsk_pcl_ros openni_remote.launch camera:=openni camera_remote:=camera_${MACHINE_NAME}"
+ssh -t -l applications ${MACHINE_NAME} "export PS1=applications;source ~/.bashrc; roslaunch jsk_pr2_startup openni_remote.launch MACHINE_NAME:=${MACHINE_NAME}"
