35e5a7df8bbd477798950536a5fda7a3b041a51a
Furushchev <furushchev@mail.ru>
2014-10-03 17:07:45 +0900

    support mongodb in move_base_db.py

--- jsk_pr2_startup/jsk_pr2_lifelog/move_base_db.py
+++ jsk_pr2_startup/jsk_pr2_lifelog/move_base_db.py
@@ -134,9 +134,98 @@ class MoveBaseDB:
                     tf2.TransformException):
                 return
 
+##########################################################
+# move_base_db that uses mongodb
+##########################################################
+
+from mongodb_store.message_store import MessageStoreProxy
+
+class MoveBaseDB_Mongo(object):
+    def __init__(self):
+        self.msg_store = MessageStoreProxy()
+        self.db_name = rospy.get_param('~db_name','__pr2_move_base_db__')
+        self.update_cycle = rospy.get_param('update_cycle', 1)
+        self.map_frame = rospy.get_param('~map_frame','map')
+        self.robot_frame = rospy.get_param('~robot_frame','base_link')
+        self.tf_listener = tf2_ros.BufferClient("tf2_buffer_server")
+        self.initialpose_pub = rospy.Publisher('/initialpose', geometry_msgs.msg.PoseWithCovarianceStamped)
+        self.current_pose = None
+        self.latest_pose = None
+        self.latest_stamp = rospy.Time(0)
+        self.load_latest_pose()
+        self.pub_latest_pose()
+
+    def insert_pose_to_db(self, map_to_robot):
+        try:
+            self.msg_store.insert_named(self.db_name, map_to_robot)
+        except Exception as e:
+            rospy.logerr('failed to insert current robot pose to db: %s', e)
+
+    def load_latest_pose(self):
+        updated = None
+        try:
+            updated = self.msg_store.query_named(self.db_name, 'geometry_msgs/TransformStamped')
+        except Exception as e:
+            rospy.logerr('failed to load latest pose from db: %s' % e)
+            return
+        if updated is not None:
+            self.current_pose = updated[0]
+            self.latest_pose = updated[0]
+
+    def need_update_db(self, t):
+        if self.current_pose is None:
+            return False
+        diffthre = 0.1 + 1.0 / (rospy.Time.now() - self.latest_stamp).to_sec()
+        diffpos = sum([(t.transform.translation.x - self.current_pose.transform.translation.x) ** 2,
+                       (t.transform.translation.y - self.current_pose.transform.translation.y) ** 2,
+                       (t.transform.translation.z - self.current_pose.transform.translation.z) ** 2]) ** 0.5
+        diffrot = sum([(t.transform.rotation.rotation.x - self.current_pose.transform.rotation.x) ** 2,
+                       (t.transform.rotation.rotation.y - self.current_pose.transform.rotation.y) ** 2,
+                       (t.transform.rotation.rotation.z - self.current_pose.transform.rotation.z) ** 2,
+                       (t.transform.rotation.rotation.w - self.current_pose.transform.rotation.w) ** 2]) ** 0.5
+        if diffthre < diffpos or diffthre / 2.0 < diffrot:
+            return True
+        else:
+            return False
+
+    def insert_current_pose(self):
+        try:
+            transform = self.tf_listener.lookup_transform(self.map_frame,self.robot_frame,rospy.Time(0))
+        if self.need_update_db(transform):
+            self.insert_pose_to_db(transform)
+            self.current_pose = transform
+        except (tf2.LookupException, tf2.ConnectivityException, \
+                tf2.ExtrapolationException, tf2.TimeoutException, \
+                tf2.TransformException) as e:
+            rospy.logwarn('failed to get current robot pose from tf: %s' % e)
+
+    def sleep_one_cycle(self):
+        self.pub_latest_pose()
+        rospy.sleep(self.update_cycle)
+
+    def pub_latest_pose(self):
+        if(self.latest_pose is not None and self.initialpose_pub.get_num_connections() > 0):
+            pub_msg = geometry_msgs.msg.PoseWithCovarianceStamped()
+            pub_msg.header.stamp = rospy.Time(0)
+            pub_msg.header.frame_id = '/map'
+            pub_msg.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
+            pub_msg.pose.pose.position.x = self.latest_pose.transform.translation.x
+            pub_msg.pose.pose.position.y = self.latest_pose.transform.translation.y
+            pub_msg.pose.pose.position.z = self.latest_pose.transform.translation.z
+            pub_msg.pose.pose.orientation = self.latest_pose.transform.rotation
+            try:
+                self.initialpose_pub.publish(pub_msg)
+                self.latest_pose = None
+            except Exception as e:
+                rospy.logerr('failed to publish initial robot pose: %s' % e)
+
 if __name__ == "__main__":
     rospy.init_node('move_base_db')
-    obj = MoveBaseDB()
+    mongodbp = rospy.get_param('~use_mongodb', False)
+    if mongodbp:
+        obj = MoveBaseDB_Mongo()
+    else:
+        obj = MoveBaseDB()
     while not rospy.is_shutdown():
         obj.insert_current_pose();
         obj.sleep_one_cycle();
