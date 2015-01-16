a8c9970c045730dcf5e8ec118e94e2a5bfe5f714
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-01-16 18:05:22 +0900

    add jsk_pr2_teleop

--- jsk_pr2_robot/jsk_pr2_startup/catkin.cmake
+++ jsk_pr2_robot/jsk_pr2_startup/catkin.cmake
@@ -1,7 +1,22 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(jsk_pr2_startup)
-find_package(catkin REQUIRED)
-catkin_package()
+find_package(catkin REQUIRED COMPONENTS
+  std_msgs
+  message_generation)
+
+add_message_files(
+  FILES
+  AngleVectorCompressed.msg
+)
+
+generate_messages(
+  DEPENDENCIES
+  std_msgs
+)
+
+catkin_package(
+  CATKIN_DEPENDS std_msgs message_runtime
+)
 
 install(DIRECTORY config jsk_pr2_image_transport
   jsk_pr2_joy jsk_pr2_lifelog jsk_pr2_move_base 	jsk_pr2_moveit
