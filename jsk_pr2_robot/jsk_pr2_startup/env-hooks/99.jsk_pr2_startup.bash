57416c2b9cae9a63fcb250d8e1b1ccfbc410c5a5
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-06-18 13:41:15 +0900

    [jsk_pr2_start_up] set ROBOT=pr2 in rossetpr10XX

--- jsk_pr2_robot/jsk_pr2_startup/env-hooks/99.jsk_pr2_startup.bash
+++ jsk_pr2_robot/jsk_pr2_startup/env-hooks/99.jsk_pr2_startup.bash
@@ -3,18 +3,18 @@
 
 function rossetpr1040() {
     rossetmaster pr1040.jsk.imi.i.u-tokyo.ac.jp
-    export ROBOT=PR2
+    export ROBOT=pr2
     rossetip
 }
 
 function rossetpr1012() {
     rossetmaster pr1012.jsk.imi.i.u-tokyo.ac.jp
-    export ROBOT=PR2
+    export ROBOT=pr2
     rossetip
 }
 
 function rossetc1() {
     rossetmaster c1
-    export ROBOT=PR2
+    export ROBOT=pr2
     rossetip
 }
