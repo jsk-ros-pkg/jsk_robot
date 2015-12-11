c0ef616342352249763e81970f4125ffe075fccb
iori <iori@jsk.imi.i.u-tokyo.ac.jp>
2015-12-11 13:57:45 +0900

    [jsk_robot_startup] Fix camera offset calculation

--- jsk_robot_common/jsk_robot_startup/scripts/CameraToBaseOffset.py
+++ jsk_robot_common/jsk_robot_startup/scripts/CameraToBaseOffset.py
@@ -43,33 +43,35 @@ class CameraToBaseOffset(object):
     def source_odom_callback(self, msg):
         with self.lock:
             # calculate camera transform
-            current_base_to_camera = self.calculate_base_to_sensor_transform()
+            current_camera_to_base = self.calculate_camera_to_base_transform()
+            if self.initial_matrix == None:
+                self.initial_matrix = current_camera_to_base
+
             if self.initial_matrix != None:
-                self.initial_matrix = current_base_to_camera
-            camera_transformation = numpy.dot(numpy.linalg.inv(self.initial_matrix), current_base_to_camera)
+                camera_relative_base_transformation = numpy.dot(numpy.linalg.inv(self.initial_matrix), current_camera_to_base) # base_link transformation in camera coords
+    
+                # calculate offseted odometry
+                source_odom_matrix = self.make_homogeneous_matrix([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z],
+                                                                  [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+                                                                   msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+                new_odom_matrix = camera_relative_base_transformation.dot(source_odom_matrix)
 
-            # calculate offseted odometry
-            source_odom_matrix = self.make_homogeneous_matrix([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z],
-                                                              [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
-                                                               msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-            new_odom_matrix = camera_transformation.dot(source_odom_matrix)
-
-            # make odometry msg. twist is copied from source_odom
-            new_odom = copy.deepcopy(msg)
-            new_odom.header.frame_id = self.odom_frame
-            new_odom.child_frame_id = self.base_link_frame
-            new_odom.pose.pose.position = Point(*list(new_odom_matrix[:3, 3]))
-            new_odom.pose.pose.orientation = Quaternion(*list(tf.transformations.quaternion_from_matrix(new_odom_matrix)))
-            new_pose_cov_matrix = numpy.matrix(new_odom.pose.covariance).reshape(6, 6)
-            rotation_matrix = camera_transformation[:3, :3]
-            new_pose_cov_matrix[:3, :3] = (rotation_matrix.T).dot(new_pose_cov_matrix[:3, :3].dot(rotation_matrix))
-            new_pose_cov_matrix[3:6, 3:6] = (rotation_matrix.T).dot(new_pose_cov_matrix[3:6, 3:6].dot(rotation_matrix))
-            new_odom.pose.covariance = numpy.array(new_pose_cov_matrix).reshape(-1,).tolist()
+                # make odometry msg. twist is copied from source_odom
+                new_odom = copy.deepcopy(msg)
+                new_odom.header.frame_id = self.odom_frame
+                new_odom.child_frame_id = self.base_link_frame
+                new_odom.pose.pose.position = Point(*list(new_odom_matrix[:3, 3]))
+                new_odom.pose.pose.orientation = Quaternion(*list(tf.transformations.quaternion_from_matrix(new_odom_matrix)))
+                new_pose_cov_matrix = numpy.matrix(new_odom.pose.covariance).reshape(6, 6)
+                rotation_matrix = camera_relative_base_transformation[:3, :3]
+                new_pose_cov_matrix[:3, :3] = (rotation_matrix.T).dot(new_pose_cov_matrix[:3, :3].dot(rotation_matrix))
+                new_pose_cov_matrix[3:6, 3:6] = (rotation_matrix.T).dot(new_pose_cov_matrix[3:6, 3:6].dot(rotation_matrix))
+                new_odom.pose.covariance = numpy.array(new_pose_cov_matrix).reshape(-1,).tolist()
                                             
-            # publish
-            self.pub.publish(new_odom)
-            if self.publish_tf:
-                self.broadcast_transform(new_odom)
+                # publish
+                self.pub.publish(new_odom)
+                if self.publish_tf:
+                    self.broadcast_transform(new_odom)
 
     def make_homogeneous_matrix(self, trans, rot):
         homogeneous_matrix = tf.transformations.quaternion_matrix(rot)
@@ -91,14 +93,14 @@ class CameraToBaseOffset(object):
             target_frame = odom.child_frame_id
         self.broadcast.sendTransform(position, orientation, odom.header.stamp, target_frame, parent_frame)
 
-    def calculate_base_to_sensor_transform(self):
+    def calculate_camera_to_base_transform(self):
         try:
-            (trans,rot) = self.listener.lookupTransform(self.base_link_frame, self.camera_frame, rospy.Time(0))
+            (trans,rot) = self.listener.lookupTransform(self.camera_frame, self.base_link_frame, rospy.Time(0))
         except:
-            rospy.logwarn("[%s] failed to solve tf in initialize_odometry: %s to %s", rospy.get_name(), self.base_link_frame, self.camera_frame)
+            rospy.logwarn("[%s] failed to solve camera_to_base tf: %s to %s", rospy.get_name(), self.camera_frame, self.base_link_frame)
             return None
-        base_link_to_camera = self.make_homogeneous_matrix(trans, rot) # base_link -> camera
-        return base_link_to_camera
+        camera_to_base_link = self.make_homogeneous_matrix(trans, rot) # camera -> base_link
+        return camera_to_base_link
         
 if __name__ == '__main__':
     try:
