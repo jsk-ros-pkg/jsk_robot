3bc478e759b522067d3b74170b0a62726507ab4c
iori <iori@jsk.imi.i.u-tokyo.ac.jp>
2015-12-29 17:51:21 +0900

    [jsk_robot_startup] Fix topic name bugs and remap bags

--- jsk_robot_common/jsk_robot_startup/scripts/OdometryTfBroadcaster.py
+++ jsk_robot_common/jsk_robot_startup/scripts/OdometryTfBroadcaster.py
@@ -6,10 +6,9 @@ import numpy
 
 class OdometryTfBroadcaster:
     def __init__(self):
-        rospy.init_node("OdometryTfReceiver", anonymous=True)
-        self.pub = rospy.Publisher("~output", Odometry, queue_size=10)
-        self.map_to_prt_sub = rospy.Publisher("~map_to_particle", TransformStamped, self.broadcast_tf)
-        self.prt_to_odom_sub = rospy.Publisher("~particle_to_odom", TransformStamped, self.broadcast_tf)
+        rospy.init_node("OdometryTfBroadcaster", anonymous=True)
+        self.map_to_prt_sub = rospy.Subscriber("~map_to_particle", TransformStamped, self.broadcast_tf, queue_size = 1)
+        self.prt_to_odom_sub = rospy.Subscriber("~particle_to_odom", TransformStamped, self.broadcast_tf, queue_size = 1)
         self.rate = rospy.get_param("~rate", 50)
         self.br = tf.TransformBroadcaster()
         self.r = rospy.Rate(self.rate) # 10hz
@@ -17,10 +16,16 @@ class OdometryTfBroadcaster:
     def execute(self):
         rospy.spin()
 
+    # def broadcast_map_to_particle(self, msg):
+    #     self.broadcast_tf(msg)
+
+    # def broadcast_particle_to_odom(self, msg):
+    #     self.broadcast_tf(msg)
+        
     def broadcast_tf(self, msg):
         self.br.sendTransform((msg.transform.translation.x, msg.transform.translation.y, msg.transform.translation.z),
                               (msg.transform.rotation.x, msg.transform.rotation.y, msg.transform.rotation.z, msg.transform.rotation.w),
-                              msg.header.stamp, msg.header.child_frame_id, msg.header.frame_id)
+                              msg.header.stamp, msg.child_frame_id, msg.header.frame_id)
 
 if __name__ == '__main__':
     try:
--- jsk_robot_common/jsk_robot_startup/scripts/OdometryTfManager.py
+++ jsk_robot_common/jsk_robot_startup/scripts/OdometryTfManager.py
@@ -7,7 +7,6 @@ import numpy
 class OdometryTfManager:
     def __init__(self):
         rospy.init_node("SlamMapTfToOdometry", anonymous=True)
-        self.pub = rospy.Publisher("~output", Odometry, queue_size=10)
         self.map_frame = rospy.get_param("~map_frame", "map")
         self.particle_frame = rospy.get_param("~particle_frame", "biped_odom_particle")
         self.odom_frame = rospy.get_param("~odom_frame", "odom")
@@ -22,9 +21,7 @@ class OdometryTfManager:
             self.publish_transform_stamped()
             self.r.sleep()
 
-    def publish_pose_stamped(self):
-        if self.base_odom == None:
-            return
+    def publish_transform_stamped(self):
         try:
             (map_to_prt_trans,map_to_prt_rot) = self.listener.lookupTransform(self.map_frame, self.particle_frame, rospy.Time(0)) # current map->base transform
             (prt_to_odom_trans,prt_to_odom_rot) = self.listener.lookupTransform(self.particle_frame, self.odom_frame, rospy.Time(0)) # current map->base transform
@@ -37,13 +34,14 @@ class OdometryTfManager:
         map_to_prt_tf.header.frame_id = self.map_frame
         map_to_prt_tf.child_frame_id = self.particle_frame
         map_to_prt_tf.transform = Transform(Vector3(*map_to_prt_trans), Quaternion(*map_to_prt_rot))
-        map_to_prt_pub.publish(map_to_prt_tf)
+        self.map_to_prt_pub.publish(map_to_prt_tf)
 
         prt_to_odom_tf = TransformStamped()
         prt_to_odom_tf.header.stamp = stamp
         prt_to_odom_tf.header.frame_id = self.particle_frame
         prt_to_odom_tf.child_frame_id = self.odom_frame
         prt_to_odom_tf.transform = Transform(Vector3(*prt_to_odom_trans), Quaternion(*prt_to_odom_rot))
+        self.prt_to_odom_pub.publish(prt_to_odom_tf)        
 
 if __name__ == '__main__':
     try:
