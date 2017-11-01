f985df7fde0abc3167f026bfdfd904800c926662
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-11-01 17:34:43 +0900

    jsk_robot_startup: logger_base: get param from global ns

--- jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/lifelog/__init__.py
+++ jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/lifelog/__init__.py
@@ -1,3 +1,4 @@
 from .action_logger import ActionLogger
 from .base_trajectory_logger import BaseTrajectoryLogger
 from .object_detection_logger import ObjectDetectionLogger
+from .tf_logger import TFLogger
--- jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/lifelog/logger_base.py
+++ jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/lifelog/logger_base.py
@@ -8,14 +8,14 @@ from mongodb_store.message_store import MessageStoreProxy
 
 class LoggerBase(object):
     def __init__(self, db_name='jsk_robot_lifelog', col_name=None):
-        self.db_name = rospy.get_param('robot/database','jsk_robot_lifelog')
+        self.db_name = rospy.get_param('/robot/database','jsk_robot_lifelog')
         try:
             if col_name is None:
-                self.col_name = rospy.get_param('robot/name')
+                self.col_name = rospy.get_param('/robot/name')
             else:
                 self.col_name = col_name
         except KeyError as e:
-            rospy.logerr("please specify param \"robot/name\" (e.g. pr1012, olive)")
+            rospy.logerr("please specify param \"/robot/name\" (e.g. pr1012, olive)")
             exit(1)
         self.update_cycle = rospy.get_param("update_cycle", 1)
 
