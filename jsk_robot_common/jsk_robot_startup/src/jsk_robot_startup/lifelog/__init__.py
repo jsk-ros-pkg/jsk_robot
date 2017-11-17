1fb508c7f3418439f5145834d7655c68103ce377
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-11-17 16:48:56 +0900

    jsk_robot_startup: expose logger_base.LoggerBase

--- jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/lifelog/__init__.py
+++ jsk_robot_common/jsk_robot_startup/src/jsk_robot_startup/lifelog/__init__.py
@@ -1,3 +1,4 @@
+from .logger_base import LoggerBase
 from .action_logger import ActionLogger
 from .base_trajectory_logger import BaseTrajectoryLogger
 from .object_detection_logger import ObjectDetectionLogger
