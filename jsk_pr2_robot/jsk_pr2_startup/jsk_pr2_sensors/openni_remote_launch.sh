2e9ae29eb62cebe99448333f52a6d4f1bc21d79d
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-01-15 22:26:07 +0900

    jsk_pr2_startup: revert to use 'kinect_head' for kinect ns

--- jsk_pr2_robot/jsk_pr2_startup/jsk_pr2_sensors/openni_remote_launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# set +e
-MACHINE_NAME=$1
-if [ "$1" = "" ]; then MACHINE_NAME="c2"; fi
-
-source ~/.bashrc
-roslaunch jsk_pr2_startup openni_remote.launch MACHINE_NAME:=${MACHINE_NAME}
-
