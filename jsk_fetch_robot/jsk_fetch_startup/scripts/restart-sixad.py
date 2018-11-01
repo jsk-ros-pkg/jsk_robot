6b4ddb81e748bc1ab530ec5fea397b487871c7ac
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-11-01 15:19:19 +0900

    add script and upstart config file to restart sixad when PS controller fails to connect

---
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/restart-sixad.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import linecache
+import os
+from subprocess import call
+import syslog
+import time
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+log_file = '/var/log/syslog'
+BASEDIR = os.path.abspath(os.path.dirname(log_file))
+
+
+class ChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        num_lines = sum(1 for line in open(log_file))  # how many lines in the log file
+        for i in range(5):  # check latest 5 lines
+            target_line = linecache.getline(log_file, num_lines - i)
+            if ('bluetoothd' in target_line) and ("Can't find device agent" in target_line):
+                syslog.syslog(syslog.LOG_INFO, 'restart sixad service')
+                call(['service', 'sixad', 'restart'])  # restart sixad
+        linecache.clearcache()
+
+
+if __name__ in '__main__':
+    while 1:
+        event_handler = ChangeHandler()
+        observer = Observer()
+        observer.schedule(event_handler, BASEDIR, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(3)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
