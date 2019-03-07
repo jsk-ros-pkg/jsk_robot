6b6d2a0b5bbe4ef3885f94be410bb9f711ef46c6
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-03-07 18:56:16 +0900

    refactor marker_msg_from_indigo_to_kinetic

--- jsk_robot_common/jsk_robot_utils/scripts/marker_msg_from_indigo_to_kinetic.py
+++ jsk_robot_common/jsk_robot_utils/scripts/marker_msg_from_indigo_to_kinetic.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Detail of visualization_msgs/msg/Marker.msg of Indigo and that of Kinetic are a little bit different, so checksums of them are different of course. That means on Kinetic you cannot subscribe the topic of which type is visualization_msgs/Marker published on  Indigo.
-# Use this script on Kinetic when you want to subscribe the topic of which type is visualization_msgs/MarkerArray published on Indigo.
-# By executing this script, you can subscribe /marker_msg_from_indigo_to_kinetic topic, the content of which is the same with visualization_msgs/MarkerArray published on Indigo.
+# Detail of visualization_msgs/msg/Marker.msg of Indigo and that of Kinetic are
+# a little bit different, so checksums of them are different of course.
+# That means on Kinetic you cannot subscribe the topic of which type is
+# visualization_msgs/Marker published on  Indigo.
+# Use this script on Kinetic when you want to subscribe the topic of which type
+# is visualization_msgs/MarkerArray published on Indigo.
+# By executing this script, you can subscribe converted topics
+# whose content is the same with visualization_msgs/MarkerArray
+# published on Indigo.
 
 import os
 import rospy
-from visualization_msgs.msg import Marker, MarkerArray
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 
 
 class VisualizationMarkerBridgeForKinetic(object):
@@ -32,51 +39,51 @@ class VisualizationMarkerBridgeForKinetic(object):
 
     def timer_cb(self, event=None):
         all_topics = rospy.get_published_topics()
-        markers = [t[0] for t in all_topics if t[1] == "visualization_msgs/Marker" and\
-                   not t[0].endswith(self.suffix)]
-        arrays = [t[0] for t in all_topics if t[1] == "visualization_msgs/MarkerArray" and\
-                  not t[0].endswith(self.suffix)]
-        names = markers + arrays
+        topic_names = []
+        topic_types = []
+        for topic in all_topics:
+            if topic[0].endswith('_' + self.suffix):
+                continue
+            if topic[1] == 'visualization_msgs/Marker':
+                topic_names.append(topic[0])
+                topic_types.append(Marker)
+            elif topic[1] == 'visualization_msgs/MarkerArray':
+                topic_names.append(topic[0])
+                topic_types.append(MarkerArray)
 
-        for name in markers:
-            if name not in self.publishers:
-                new_name = name + "/" + self.suffix
-                self.publishers[name] = rospy.Publisher(
-                    new_name, Marker, queue_size=1)
-                rospy.logdebug("Advertised %s" % new_name)
+        for topic_name, topic_type in zip(topic_names, topic_types):
+            if topic_name not in self.publishers:
+                new_topic_name = '{0}_{1}'.format(topic_name, self.suffix)
+                self.publishers[topic_name] = rospy.Publisher(
+                    new_topic_name, topic_type, queue_size=1)
+                rospy.logdebug(
+                    'Advertised: {0} -> {1}'.format(
+                        topic_name, new_topic_name))
 
-        for name in arrays:
-            if name not in self.publishers:
-                new_name = name + "/" + self.suffix
-                self.publishers[name] = rospy.Publisher(
-                    new_name, MarkerArray, queue_size=1)
-                rospy.logdebug("Advertised %s" % new_name)
-
-        for name, pub in self.publishers.items():
+        for topic_name, pub in self.publishers.items():
             # clean old topics
-            if name not in names:
+            if topic_name not in topic_names:
                 try:
-                    self.subscribers.pop(name).unregister()
-                    rospy.logdebug("Removed sub %s" % name)
-                except:
+                    self.subscribers.pop(topic_name).unregister()
+                    rospy.logdebug('Removed subscriber: {}'.format(topic_name))
+                except Exception:
                     pass
                 try:
-                    self.publishers.pop(name).unregister()
-                    rospy.logdebug("Removed pub %s" % name)
-                except:
+                    self.publishers.pop(topic_name).unregister()
+                    rospy.logdebug('Removed publisher: {}'.format(topic_name))
+                except Exception:
                     pass
             # subscribe topics subscribed
             elif pub.get_num_connections() > 0:
-                if name not in self.subscribers:
-                    self.subscribers[name] = rospy.Subscriber(
-                        name, rospy.AnyMsg,
-                        self.msg_cb, name)
-                    rospy.logdebug("Subscribed %s" % name)
+                if topic_name not in self.subscribers:
+                    self.subscribers[topic_name] = rospy.Subscriber(
+                        topic_name, rospy.AnyMsg, self.msg_cb, topic_name)
+                    rospy.logdebug('Subscribed {}'.format(topic_name))
             # unsubscribe topics unsubscribed
             else:
-                if name in self.subscribers:
-                    self.subscribers.pop(name).unregister()
-                    rospy.logdebug("Unsubscribed %s" % name)
+                if topic_name in self.subscribers:
+                    self.subscribers.pop(topic_name).unregister()
+                    rospy.logdebug('Unsubscribed {}'.format(topic_name))
 
 
 if __name__ == '__main__':
