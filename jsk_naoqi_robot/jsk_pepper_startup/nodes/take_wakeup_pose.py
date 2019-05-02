9b80a9e51537812ced7584c572ef68f30db76727
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2019-05-02 23:50:22 +0900

    [jsk_pepper_startup] add servo on node when start jsk_pepper_startup

---
+++ jsk_naoqi_robot/jsk_pepper_startup/nodes/take_wakeup_pose.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+disable AutonomousLife and servo on
+'''
+
+import rospy
+from std_srvs.srv import (
+    EmptyResponse,
+    Empty,
+    Trigger)
+
+def get_life_state():
+    rospy.wait_for_service('/pepper_robot/pose/life/get_state')
+    get_life_proxy = rospy.ServiceProxy('/pepper_robot/pose/life/get_state', Trigger)
+    res_get_life = get_life_proxy()
+    return res_get_life.message
+
+def disable_life():
+    rospy.wait_for_service('/pepper_robot/pose/life/disable')
+    disable_life_proxy = rospy.ServiceProxy('/pepper_robot/pose/life/disable', Empty)
+    disable_life_proxy()
+    return EmptyResponse()
+
+def servo_on():
+    rospy.wait_for_service('/pepper_robot/pose/wakeup')
+    servo_on_proxy = rospy.ServiceProxy('/pepper_robot/pose/wakeup', Empty)
+    servo_on_proxy()
+    return EmptyResponse()
+
+def take_initial_pose():
+    try:
+        # check current AutonomousLife status
+        life_state = get_life_state()
+        # set AutonomousLife disabled
+        if not(life_state == "disabled"):
+            disable_life()
+        # servo on
+        servo_on()
+    except RuntimeError, e:
+        rospy.logerr("Exception caught:\n%s", e)
+
+if __name__=="__main__":
+    rospy.init_node("take_initial_pose")
+    take_initial_pose()
