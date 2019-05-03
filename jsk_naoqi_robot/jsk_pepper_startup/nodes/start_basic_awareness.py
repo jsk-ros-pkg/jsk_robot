986665fbd343876488bee672d528e374685f7a40
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2019-05-03 11:50:26 +0900

    add basic awareness node

---
+++ jsk_naoqi_robot/jsk_pepper_startup/nodes/start_basic_awareness.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from subprocess import *
+from std_srvs.srv import (
+    SetBoolResponse,
+    SetBool)
+
+def start_basic_awareness_service(req):
+    rospy.wait_for_service('/basic_awareness/set_enabled')
+    basic_awareness_proxy = rospy.ServiceProxy('/basic_awareness/set_enabled', SetBool)
+    basic_awareness_proxy(req)
+    return SetBoolResponse()
+
+def start_basic_awareness(req):
+    # call set_enabled service
+    start_basic_awareness_service(req)
+    # kill basic awareness node
+    call(['rosnode', 'kill', '/naoqi_basic_awareness'])
+
+if __name__ == "__main__":
+    rospy.init_node("start_basic_awareness")
+    start_basic_awareness(True)
