ad06dd883cd603a0756d15bf4151fa9cf1ee5b87
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2019-05-09 16:10:30 +0900

    [jsk_pepper_startup] add autonomous_enabled argument

---
+++ jsk_naoqi_robot/jsk_pepper_startup/nodes/start_background_movement.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from subprocess import *
+from std_srvs.srv import (
+    SetBoolResponse,
+    SetBool)
+
+def start_background_movement_service(req):
+    rospy.wait_for_service('/background_movement/set_enabled')
+    background_movement_proxy = rospy.ServiceProxy('/background_movement/set_enabled', SetBool)
+    background_movement_proxy(req)
+    return SetBoolResponse()
+
+def start_background_movement(req):
+    # call set_enabled service
+    start_background_movement_service(req)
+    # kill background movement node
+    call(['rosnode', 'kill', '/naoqi_background_movement'])
+
+if __name__ == "__main__":
+    rospy.init_node("start_background_movement")
+    start_background_movement(True)
