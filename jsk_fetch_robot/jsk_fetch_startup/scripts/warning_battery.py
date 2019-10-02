d0d1f06cba5ee7547bfb9d5b45c4d6ff6920e5ad
HiroIshida <spitfire.docomo@gmail.com>
2019-10-02 22:49:17 +0900

    create class

--- jsk_fetch_robot/jsk_fetch_startup/scripts/warning_battery.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/warning_battery.py
@@ -3,38 +3,39 @@
 import actionlib
 import rospy
 from time import sleep
-
 from sound_play.msg import SoundRequestAction
 from sound_play.msg import SoundRequest, SoundRequestGoal
 from power_msgs.msg import BatteryState
 
-rospy.init_node("battery_warning")
+class BatteryWarning:
+    def __init__(self, threshould):
+        self.speak_client = actionlib.SimpleActionClient("/robotsound_jp", SoundRequestAction)
+        self.charge_level_percentage = 100.0
+        self.threshould = threshould 
+        self.subscriber = rospy.Subscriber("/battery_state", BatteryState, self.callback)
 
-speak_client = actionlib.SimpleActionClient("/robotsound_jp", SoundRequestAction)
-waitEnough = speak_client.wait_for_server(rospy.Duration(10))
+    def _speak(self, sentence):
+        req = SoundRequest()
+        req.command = SoundRequest.PLAY_ONCE
+        req.sound = SoundRequest.SAY
+        req.arg = sentence
+        req.arg2 = "ja"
+        req.volume = 1.0
+        self.speak_client.send_goal(SoundRequestGoal(sound_request=req))
+        self.speak_client.wait_for_result(timeout=rospy.Duration(10))
 
-def speak(sentence):
-    req = SoundRequest()
-    req.command = SoundRequest.PLAY_ONCE
-    req.sound = SoundRequest.SAY
-    req.arg = sentence
-    req.arg2 = "ja"
-    req.volume = 1.0
-    speak_client.send_goal(SoundRequestGoal(sound_request=req))
-    speak_client.wait_for_result(timeout=rospy.Duration(10))
+    def callback(self, msg):
+        self.charge_level = msg.charge_level * 100
+        if self.charge_level < self.threshould:
+            print msg.charge_level * 100
+            sentence = "バッテリー残り" + str(int(self.charge_level)) + "パーセント"  
+            self._speak(sentence)
+            sleep(0.2)
+            self._speak("もう限界ですので、僕をお家にかえしてください")
 
-global charge_level
-charge_level = None
-def callback(msg):
-    global charge_level
-    charge_level = int(msg.charge_level * 100)
-    if charge_level < 70:
-        sentence = "バッテリー残り" + str(charge_level) + "パーセント"
-        speak(sentence)
-        sleep(0.2)
-        speak("僕をお家にかえして")
-        sleep(0.2)
-
-rospy.Subscriber("/battery_state", BatteryState, callback)
-rospy.spin()
+if __name__ == '__main__':
+    rospy.init_node("battery_warning")
+    charge_level_threshould = 80.0 # percent
+    bw = BatteryWarning(charge_level_threshould)
+    rospy.spin()
 
