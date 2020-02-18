d8f7cff27e88eb1a55b6f2b8c09aaf62f2e8f6f7
Naoya Yamaguchi <708yamaguchi@gmail.com>
2020-02-18 15:01:59 +0900

    [fetch] script and upstart config to use touchscreen vertically

--- jsk_fetch_robot/jsk_fetch_startup/config/install_upstart.sh
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_upstart.sh
@@ -5,6 +5,7 @@ jsk_fetch_startup=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`"/.. > /dev/null &&
 IFS=':' read -r -a prefix_paths <<< "$CMAKE_PREFIX_PATH"
 current_prefix_path="${prefix_paths[0]}"
 
+# jsk-fetch-startup
 sudo bash -c "cat <<EOF > /etc/init/jsk-fetch-startup.conf
 description \"spawn jsk bringup nodes for fetch\"
 # use default log dir: yamaguchi & s-kitagawa (2019/04/18)
@@ -36,3 +37,23 @@ EOF"
 echo "== cat /etc/init/jsk-fetch-startup.conf =="
 cat /etc/init/jsk-fetch-startup.conf
 echo "== usage: sudo service jsk-fetch-startup restart =="
+
+# vertical-touchscreen
+sudo bash -c "cat <<EOF > /etc/init/vertical-touchscreen.conf
+# See https://askubuntu.com/questions/507496/how-to-start-gui-application-with-upstart
+description \"Use vertical touchscreen\"
+
+start on runlevel [2345]
+stop on runlevel [016]
+
+script
+    sleep 30 # Wait for X programs to start
+    export DISPLAY=:0
+    exec su fetch -c \". ${current_prefix_path}/setup.bash && ${jsk_fetch_startup}/scripts/vertical-touchscreen.sh\"
+end script
+
+EOF"
+echo
+echo "== cat /etc/init/vertical-touchscreen.conf =="
+cat /etc/init/vertical-touchscreen.conf
+echo "== usage: sudo service vertical-touchscreen restart =="
