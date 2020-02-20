da1c95b907f30635911f7db0e664fcbc914449ab
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-02-20 20:31:38 +0900

    fix escape evaluation in install_upstart.sh

--- jsk_fetch_robot/jsk_fetch_startup/config/install_upstart.sh
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_upstart.sh
@@ -5,8 +5,8 @@ jsk_fetch_startup=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`"/.. > /dev/null &&
 IFS=':' read -r -a prefix_paths <<< "$CMAKE_PREFIX_PATH"
 current_prefix_path="${prefix_paths[0]}"
 
-sudo bash -c "cat <<EOF > /etc/init/jsk-fetch-startup.conf
-description \"spawn jsk bringup nodes for fetch\"
+# jsk-fetch-startup
+sudo bash -c "echo \"description \\\"spawn jsk bringup nodes for fetch\\\"
 # use default log dir: yamaguchi & s-kitagawa (2019/04/18)
 # env ROS_LOG_DIR=/var/log/ros
 
@@ -16,23 +16,21 @@ stop on roscore_is_down
 respawn
 
 # add ROSCONSOLE_FORMAT: s-kitagawa (2019/10/03)
-env AUDIO_DEVICE=alsa_output.usb-1130_USB_AUDIO-00-AUDIO.analog-stereo
-env ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}:${logger}]: ${message}'
+env AUDIO_DEVICE=\\\"alsa_output.usb-1130_USB_AUDIO-00-AUDIO.analog-stereo\\\"
+env ROSCONSOLE_FORMAT=\\\"[\\\${severity}] [\\\${time}] [\\\${node}:\\\${logger}]: \\\${message}\\\"
 
 # enable usb speaker if available
 pre-start script
     # launch by fetch user: yamaguchi & s-kitagawa (2019/04/18)
-    # exec su ros -c 'pactl set-default-sink $AUDIO_DEVICE || true'
-    exec su fetch -c 'pactl set-default-sink $AUDIO_DEVICE || true'
+    # exec su ros -c \\\"pactl set-default-sink \\\$AUDIO_DEVICE || true\\\"
+    exec su fetch -c \\\"pactl set-default-sink \\\$AUDIO_DEVICE || true\\\"
 end script
 
 script
     # launch by fetch user: yamaguchi & s-kitagawa (2019/04/18)
-    # exec su ros -c \". ${current_prefix_path}/setup.bash && roslaunch ${jsk_fetch_startup}/launch/fetch_bringup.launch boot_sound:=true\"
-    exec su fetch -c \". ${current_prefix_path}/setup.bash && roslaunch ${jsk_fetch_startup}/launch/fetch_bringup.launch boot_sound:=true\"
-end script
-
-EOF"
+    # exec su ros -c \\\". ${current_prefix_path}/setup.bash && roslaunch ${jsk_fetch_startup}/launch/fetch_bringup.launch boot_sound:=true\\\"
+    exec su fetch -c \\\". ${current_prefix_path}/setup.bash && roslaunch ${jsk_fetch_startup}/launch/fetch_bringup.launch boot_sound:=true\\\"
+end script\" > /etc/init/jsk-fetch-startup.conf"
 echo "== cat /etc/init/jsk-fetch-startup.conf =="
 cat /etc/init/jsk-fetch-startup.conf
 echo "== usage: sudo service jsk-fetch-startup restart =="
