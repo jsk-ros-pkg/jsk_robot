371fca602882d09d39041bfa08124c7e2c8ee5f4
jsk-fetchuser <fetch@jsk.imi.i.u-tokyo.ac.jp>
2020-06-24 13:40:08 +0900

    update odom yaw covariance

--- jsk_fetch_robot/jsk_fetch_startup/scripts/odom_corrector.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/odom_corrector.py
@@ -17,7 +17,7 @@ class OdomCorrector(object):
                            0, 0, 0, 0, 0, 0,
                            0, 0, 0, 0, 0, 0,
                            0, 0, 0, 0, 0, 0,
-                           0, 0, 0, 0, 0, 1e-3]
+                           0, 0, 0, 0, 0, 0.1]
 
     def _cb(self, msg):
         if all([x == 0 for x in msg.pose.covariance]):
