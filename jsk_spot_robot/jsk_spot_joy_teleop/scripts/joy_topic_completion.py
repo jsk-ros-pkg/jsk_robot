8c1e4ee3747eddb44be889b899907cee9ec91bb2
Koki Shinjo <sktometometo@gmail.com>
2020-11-03 00:42:30 +0900

    [jsk_spot_joy_teleop] fix format of joy_topic_completion.py

--- jsk_spot_robot/jsk_spot_joy_teleop/scripts/joy_topic_completion.py
+++ jsk_spot_robot/jsk_spot_joy_teleop/scripts/joy_topic_completion.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import rospy
 from sensor_msgs.msg import Joy
  
@@ -17,13 +19,13 @@ class JoyTopicCompletion:
 
     def __init__(self):
 
-	rospy.init_node('joy_topic_completion')
-	self.sub = rospy.Subscriber("~joy_input", Joy, self.callback)
+        rospy.init_node('joy_topic_completion')
+        self.sub = rospy.Subscriber("~joy_input", Joy, self.callback)
         self.pub = rospy.Publisher('~joy_output', Joy, queue_size=10)
         self.enable_button = int(rospy.get_param('~enable_button', 0))
- 
+
+
 if __name__ == '__main__':
 
     joy_topic_completion = JoyTopicCompletion()
     rospy.spin()
-
