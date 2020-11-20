7ec30254c3f670cf0b57b3b1d141e7d4e61801c7
Koki Shinjo <sktometometo@gmail.com>
2020-11-20 20:51:20 +0900

    [jsk_spot_teleop] add stop service and fix button bug

--- jsk_spot_robot/jsk_spot_teleop/src/joystick_teleop.cpp
+++ jsk_spot_robot/jsk_spot_teleop/src/joystick_teleop.cpp
@@ -15,8 +15,16 @@ public:
         ros::param::param<int>("~button_self_right", button_self_right_, -1);
         ros::param::param<int>("~button_sit", button_sit_, -1);
         ros::param::param<int>("~button_stand", button_stand_, -1);
+        ros::param::param<int>("~button_stop", button_stop_, -1);
         ros::param::param<int>("~button_release", button_release_, -1);
         ros::param::param<int>("~button_claim", button_claim_, -1);
+
+        ros::param::param<int>("~num_buttons", num_buttons_, 0);
+        num_buttons_ = num_buttons_ < 0 ? 0 : num_buttons_;
+        pressed_.resize(num_buttons_);
+        for ( auto index: pressed_ ) {
+            index = false;
+        }
 
         client_estop_hard_ = nh.serviceClient<std_srvs::Trigger>("/spot/estop/hard");
         client_estop_gentle_ = nh.serviceClient<std_srvs::Trigger>("/spot/estop/gentle");
@@ -25,6 +33,7 @@ public:
         client_self_right_ = nh.serviceClient<std_srvs::Trigger>("/spot/self_right");
         client_sit_ = nh.serviceClient<std_srvs::Trigger>("/spot/sit");
         client_stand_ = nh.serviceClient<std_srvs::Trigger>("/spot/stand");
+        client_stop_ = nh.serviceClient<std_srvs::Trigger>("/spot/stop");
         client_release_ = nh.serviceClient<std_srvs::Trigger>("/spot/release");
         client_claim_ = nh.serviceClient<std_srvs::Trigger>("/spot/claim");
 
@@ -36,126 +45,220 @@ public:
         std_srvs::Trigger srv;
 
         // estop_hard
-        if ( ( button_estop_hard_ >= 0 and button_estop_hard_ < msg->buttons.size() ) ) {
+        if ( button_estop_hard_ >= 0
+                and button_estop_hard_ < msg->buttons.size()
+                and button_estop_hard_ < num_buttons_ ) {
             if ( msg->buttons[button_estop_hard_] == 1 ) {
-                if ( client_estop_hard_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'estop_hard' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'estop_hard' failed.");
+                if ( not pressed_[button_estop_hard_] ) {
+                    if ( client_estop_hard_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'estop_hard' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'estop_hard' failed.");
+                    }
+                    pressed_[button_estop_hard_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_estop_hard_] ) {
+                    pressed_[button_estop_hard_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'estop_hard' is disabled.");
         }
 
         // estop_gentle
-        if ( ( button_estop_gentle_ >= 0 and button_estop_gentle_ < msg->buttons.size() ) ) {
+        if ( button_estop_gentle_ >= 0
+                and button_estop_gentle_ < msg->buttons.size()
+                and button_estop_gentle_ < num_buttons_ ) {
             if ( msg->buttons[button_estop_gentle_] == 1 ) {
-                if ( client_estop_gentle_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'estop_gentle' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'estop_gentle' failed.");
+                if ( not pressed_[button_estop_gentle_] ) {
+                    if ( client_estop_gentle_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'estop_gentle' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'estop_gentle' failed.");
+                    }
+                    pressed_[button_estop_gentle_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_estop_gentle_] ) {
+                    pressed_[button_estop_gentle_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'estop_gentle' is disabled.");
         }
 
         // power_off
-        if ( ( button_power_off_ >= 0 and button_power_off_ < msg->buttons.size() ) ) {
+        if ( button_power_off_ >= 0
+                and button_power_off_ < msg->buttons.size()
+                and button_power_off_ < num_buttons_ ) {
             if ( msg->buttons[button_power_off_] == 1 ) {
-                if ( client_power_off_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'power_off' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'power_off' failed.");
+                if ( not pressed_[button_power_off_] ) {
+                    if ( client_power_off_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'power_off' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'power_off' failed.");
+                    }
+                    pressed_[button_power_off_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_power_off_] ) {
+                    pressed_[button_power_off_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'power_off' is disabled.");
         }
 
         // power_on
-        if ( ( button_power_on_ >= 0 and button_power_on_ < msg->buttons.size() ) ) {
+        if ( button_power_on_ >= 0
+                and button_power_on_ < msg->buttons.size()
+                and button_power_on_ < num_buttons_ ) {
             if ( msg->buttons[button_power_on_] == 1 ) {
-                if ( client_power_on_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'power_on' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'power_on' failed.");
+                if ( not pressed_[button_power_on_] ) {
+                    if ( client_power_on_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'power_on' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'power_on' failed.");
+                    }
+                    pressed_[button_power_on_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_power_on_] ) {
+                    pressed_[button_power_on_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'power_on' is disabled.");
         }
 
         // self_right
-        if ( ( button_self_right_ >= 0 and button_self_right_ < msg->buttons.size() ) ) {
+        if ( button_self_right_ >= 0
+                and button_self_right_ < msg->buttons.size()
+                and button_self_right_ < num_buttons_ ) {
             if ( msg->buttons[button_self_right_] == 1 ) {
-                if ( client_self_right_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'self_right' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'self_right' failed.");
+                if ( not pressed_[button_self_right_] ) {
+                    if ( client_self_right_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'self_right' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'self_right' failed.");
+                    }
+                    pressed_[button_self_right_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_self_right_] ) {
+                    pressed_[button_self_right_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'self_right' is disabled.");
         }
 
         // sit
-        if ( ( button_sit_ >= 0 and button_sit_ < msg->buttons.size() ) ) {
+        if ( button_sit_ >= 0
+                and button_sit_ < msg->buttons.size()
+                and button_sit_ < num_buttons_ ) {
             if ( msg->buttons[button_sit_] == 1 ) {
-                if ( client_sit_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'sit' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'sit' failed.");
+                if ( not pressed_[button_sit_] ) {
+                    if ( client_sit_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'sit' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'sit' failed.");
+                    }
+                    pressed_[button_sit_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_sit_] ) {
+                    pressed_[button_sit_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'sit' is disabled.");
         }
 
         // stand
-        if ( ( button_stand_ >= 0 and button_stand_ < msg->buttons.size() ) ) {
+        if ( button_stand_ >= 0
+                and button_stand_ < msg->buttons.size()
+                and button_stand_ < num_buttons_ ) {
             if ( msg->buttons[button_stand_] == 1 ) {
-                if ( client_stand_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'stand' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'stand' failed.");
+                if ( not pressed_[button_stand_] ) {
+                    if ( client_stand_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'stand' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'stand' failed.");
+                    }
+                    pressed_[button_stand_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_stand_] ) {
+                    pressed_[button_stand_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'stand' is disabled.");
         }
 
-        // release
-        if ( ( button_release_ >= 0 and button_release_ < msg->buttons.size() ) ) {
-            if ( msg->buttons[button_release_] == 1 ) {
-                if ( client_release_.call(srv) && srv.response.success ) {
-                    ROS_INFO("Service 'release' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'release' failed.");
+        // stop
+        if ( button_stop_ >= 0
+                and button_stop_ < msg->buttons.size()
+                and button_stop_ < num_buttons_ ) {
+            if ( msg->buttons[button_stop_] == 1 ) {
+                if ( not pressed_[button_stop_] ) {
+                    if ( client_stop_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'stop' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'stop' failed.");
+                    }
+                    pressed_[button_stop_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_stop_] ) {
+                    pressed_[button_stop_] = false;
+                }
+            }
+        } else {
+            ROS_DEBUG("Button 'stop' is disabled.");
+        }
+
+        // release
+        if ( button_release_ >= 0
+                and button_release_ < msg->buttons.size()
+                and button_release_ < num_buttons_ ) {
+            if ( msg->buttons[button_release_] == 1 ) {
+                if ( not pressed_[button_release_] ) {
+                    if ( client_release_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'release' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'release' failed.");
+                    }
+                    pressed_[button_release_] = true;
+                }
+            } else {
+                if ( pressed_[button_release_] ) {
+                    pressed_[button_release_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'release' is disabled.");
         }
 
         // claim
-        if ( ( button_claim_ >= 0 and button_claim_ < msg->buttons.size() ) ) {
+        if ( button_claim_ >= 0
+                and button_claim_ < msg->buttons.size()
+                and button_claim_ < num_buttons_ ) {
             if ( msg->buttons[button_claim_] == 1 ) {
-                if ( client_claim_.call(srv) ) {
-                    ROS_INFO("Service 'claim' succeeded.");
-                } else {
-                    ROS_ERROR("Service 'claim' failed.");
+                if ( not pressed_[button_claim_] ) {
+                    if ( client_claim_.call(srv) && srv.response.success ) {
+                        ROS_INFO("Service 'claim' succeeded.");
+                    } else {
+                        ROS_ERROR("Service 'claim' failed.");
+                    }
+                    pressed_[button_claim_] = true;
                 }
-                return;
+            } else {
+                if ( pressed_[button_claim_] ) {
+                    pressed_[button_claim_] = false;
+                }
             }
         } else {
             ROS_DEBUG("Button 'claim' is disabled.");
@@ -163,27 +266,32 @@ public:
     }
 
 private:
+    int button_estop_hard_;
+    int button_estop_gentle_;
+    int button_power_off_;
+    int button_power_on_;
+    int button_self_right_;
     int button_sit_;
     int button_stand_;
-    int button_self_right_;
-    int button_claim_;
+    int button_stop_;
     int button_release_;
-    int button_power_on_;
-    int button_power_off_;
-    int button_estop_gentle_;
-    int button_estop_hard_;
+    int button_claim_;
 
+    ros::ServiceClient client_estop_hard_;
+    ros::ServiceClient client_estop_gentle_;
+    ros::ServiceClient client_power_off_;
+    ros::ServiceClient client_power_on_;
+    ros::ServiceClient client_self_right_;
     ros::ServiceClient client_sit_;
     ros::ServiceClient client_stand_;
-    ros::ServiceClient client_self_right_;
-    ros::ServiceClient client_claim_;
+    ros::ServiceClient client_stop_;
     ros::ServiceClient client_release_;
-    ros::ServiceClient client_power_on_;
-    ros::ServiceClient client_power_off_;
-    ros::ServiceClient client_estop_gentle_;
-    ros::ServiceClient client_estop_hard_;
+    ros::ServiceClient client_claim_;
 
     ros::Subscriber sub_joy_;
+
+    int num_buttons_;
+    std::vector<bool> pressed_;
 };
 
 
