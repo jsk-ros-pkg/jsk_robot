b54175ce5281d2012d506f7b929c480831730a3d
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2020-12-03 22:33:35 +0900

    add odom ground publisher

---
+++ jsk_robot_common/jsk_robot_startup/scripts/odom_ground_publisher.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy
+from std_msgs.msg import Empty as EmptyMsg
+import tf
+import time
+
+def callback(msg):
+    rospy.loginfo("reset odom_ground")
+    try:
+        stamp = rospy.Time(0)
+        (l_trans,l_rot) = tfl.lookupTransform(odom_frame, "lleg_end_coords", stamp)
+        (r_trans,r_rot) = tfl.lookupTransform(odom_frame, "rleg_end_coords", stamp)
+        position = numpy.mean([l_trans, r_trans], axis=0)
+        orientation = [0, 0, 0, 1]
+    except:
+        rospy.logerr("Failed to reset {} frame".format(odom_ground_frame))
+        return
+
+if __name__ == "__main__":
+    rospy.init_node("odom_ground_publisher")
+    sub = rospy.Subscriber("/odom_init_trigger", EmptyMsg, callback)
+    tfl = tf.TransformListener(True, rospy.Duration(10))
+    tfb = tf.TransformBroadcaster()
+    odom_frame = rospy.get_param("~odom_frame", "odom")
+    odom_ground_frame = rospy.get_param("~odom_ground_frame", "odom_ground")
+    rate = rospy.get_param("~rate", 1)
+    r = rospy.Rate(rate)
+
+    position = None
+    orientation = None
+
+    while not rospy.is_shutdown():
+        if position is not None:
+            try:
+                tfb.sendTransform(position, orientation, rospy.Time(0), odom_ground_frame, odom_frame)
+            except:
+                rospy.logerr("Failed to broadcast {} to {} transform".format(odom_frame, odom_ground_frame))
+        else:
+            print("make {} frame".format(odom_ground))
+            dummy_msg = EmptyMsg()
+            callback(dummy_msg)
+            time.sleep(1) # wait to make odom ground tf
+        r.sleep()
