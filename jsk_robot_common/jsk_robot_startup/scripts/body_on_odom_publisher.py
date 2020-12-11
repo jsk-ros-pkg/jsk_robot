df3bc0ceed0be2eaced6a460c1dc03e68a4dc140
Yuki Omori <ohmori@jsk.imi.i.u-tokyo.ac.jp>
2020-12-11 19:38:25 +0900

    add body_on_odom publisher

---
+++ jsk_robot_common/jsk_robot_startup/scripts/body_on_odom_publisher.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+import time
+
+def update_position():
+    global position
+    global orientation
+    global stamp
+
+    rospy.logdebug("update body_on_odom")
+    try:
+        # stamp = rospy.Time(0)
+        tfl.waitForTransform(robot_frame, odom_ground_frame, stamp, rospy.Duration(1.0))
+        (trans, rot) = tfl.lookupTransform(robot_frame, odom_ground_frame, stamp)
+        position = [0, 0, trans[2]]
+        orientation = [0, 0, 0, 1]
+    except:
+        rospy.logerr("Failed to update body_on_odom")
+        return
+
+if __name__ == "__main__":
+    rospy.init_node("body_on_odom_publisher")
+    tfl = tf.TransformListener(True, rospy.Duration(10))
+    tfb = tf.TransformBroadcaster()
+    robot_frame = rospy.get_param("~robot_frame", "BODY")
+    rospy.loginfo("robot frame: {}".format(robot_frame))
+    odom_ground_frame = rospy.get_param("~odom_ground_frame", "odom_ground")
+    rospy.loginfo("odom ground frame: {}".format(odom_ground_frame))
+    rate = rospy.get_param("~rate", 100)
+    r = rospy.Rate(rate)
+
+    position = None
+    orientation = None
+
+    while not rospy.is_shutdown():
+        stamp = rospy.Time.now()
+        update_position()
+        try:
+            tfb.sendTransform(position, orientation, stamp, "body_on_odom", robot_frame)
+            # tfb.sendTransform(position, orientation, rospy.Time(0), "body_on_odom", robot_frame)
+        except:
+            rospy.logerr("Failed to broadcast {} to {} transform".format("body_on_odom", robot_frame))
+            time.sleep(1)
+        r.sleep()
