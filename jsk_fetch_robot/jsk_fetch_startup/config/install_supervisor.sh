6ca6e4f3c538cba1e21492cd064e842565fc3594
Naoya Yamaguchi <708yamaguchi@gmail.com>
2021-02-17 21:18:00 +0900

    Add script to copy supervisor config files to /etc/supervisor/conf.d

---
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_supervisor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+jsk_fetch_startup=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`"/.. > /dev/null && pwd)
+
+IFS=':' read -r -a prefix_paths <<< "$CMAKE_PREFIX_PATH"
+current_prefix_path="${prefix_paths[0]}"
+
+#
+cd $jsk_fetch_startup/supervisor_scripts
+for file in $(ls ./*.conf); do
+    sudo cp $file /etc/supervisor/conf.d/
+    sudo chown root:root /etc/supervisor/conf.d/$file
+    sudo chmod 644 /etc/supervisor/conf.d/$file
+    echo "copied $file to /etc/supervisor/conf.d"
+done
