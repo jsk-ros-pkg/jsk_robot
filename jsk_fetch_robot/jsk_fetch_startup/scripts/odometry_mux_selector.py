8468a7114dee885f2122a1659df41064517d357d
Koki Shinjo <sktometometo@gmail.com>
2021-02-18 16:33:29 +0900

    [jsk_fetch_startup] add odometry_mux_selector.py

---
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/odometry_mux_selector.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+import rostopic
+
+from sensor_msgs.msg import Odometry
+from topic_tools.srv import MuxSelectRequest
+
+class OdometryMuxSelector(object):
+
+    def __init__(self):
+
+        try:
+            self._rate_threshold = float(rospy.get_param('~rate_threshold', 10))
+            self._topic_odom_initial = str(rospy.get_param('~topic_odom_initial', ''))
+            self._topic_odom_backup = str(rospy.get_param('~topic_odom_backup', ''))
+            self._topic_tf_initial = str(rospy.get_param('~topic_tf_initial', ''))
+            self._topic_tf_backup = str(rospy.get_param('~topic_tf_backup', ''))
+        except Exception as e:
+            rospy.logerr('Error:{}'.format(e))
+
+        rospy.wait_for_service('~select_service_topic')
+        rospy.wait_for_service('~select_service_tf')
+        self._srv_select_topic = rospy.ServiceProxy('~select_service_topic', MuxSelectRequest)
+        self._srv_select_tf = rospy.ServiceProxy('~select_service_tf', MuxSelectRequest)
+
+        self.r = rostopic.ROSTopicHz(-1)
+        rospy.Subscriber(self._topic_odom_initial, rospy.AnyMsg, self.r.callback_hz, callback_args=self._topic_odom_initial)
+
+    def select(self, topic_odom, topic_tf):
+
+        try:
+            self._srv_topic(topic_odom)
+            self._srv_tf(topic_tf)
+        except (rospy.ServiceException, rospy.ROSException) as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+
+    def spin(self):
+
+        rate = rospy.sleep(1)
+        while not rospy.is_shutdown():
+            rate.sleep()
+            if self.r.get_hz(self._topic_odom_initial)[0] < self._rate_threshold:
+                self.select( self._topic_odom_backup, self._topic_tf_backup )
+
+def main():
+
+    rospy.init_node('odometry_mux_selector')
+    selector = OdometryMuxSelector()
+    selector.spin()
+
+if __name__ == '__main__':
+    main()
