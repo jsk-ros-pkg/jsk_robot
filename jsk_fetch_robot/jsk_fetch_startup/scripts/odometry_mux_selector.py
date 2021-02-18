99606cabeb1983395fb800fcbbe27b0e51566a16
JSK fetch user <fetch@jsk.imi.i.u-tokyo.ac.jp>
2021-02-18 23:49:54 +0900

    [jsk_fetch_startup] fix bugs in odometry_mux_selector.py

--- jsk_fetch_robot/jsk_fetch_startup/scripts/odometry_mux_selector.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/odometry_mux_selector.py
@@ -1,51 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 import rospy
 import rostopic
 
-from topic_tools.srv import MuxSelectRequest
+from topic_tools.srv import MuxSelect, MuxSelectRequest
+from nav_msgs.msg import Odometry
 
 class OdometryMuxSelector(object):
 
     def __init__(self):
 
         try:
-            self._rate_threshold = float(rospy.get_param('~rate_threshold', 10))
             self._topic_odom_initial = str(rospy.get_param('~topic_odom_initial', ''))
             self._topic_odom_backup = str(rospy.get_param('~topic_odom_backup', ''))
             self._topic_tf_initial = str(rospy.get_param('~topic_tf_initial', ''))
             self._topic_tf_backup = str(rospy.get_param('~topic_tf_backup', ''))
         except Exception as e:
             rospy.logerr('Error:{}'.format(e))
+            sys.exit(1)
 
-        rospy.wait_for_service('~select_service_topic')
-        rospy.wait_for_service('~select_service_tf')
-        self._srv_select_topic = rospy.ServiceProxy('~select_service_topic', MuxSelectRequest)
-        self._srv_select_tf = rospy.ServiceProxy('~select_service_tf', MuxSelectRequest)
+        try:
+            rospy.wait_for_service('~select_service_topic', 10.0)
+            rospy.wait_for_service('~select_service_tf', 10.0)
+        except rospy.ROSException as e:
+            rospy.logerr('Service is not found:{}'.format(e))
+            sys.exit(1)
+
+        try:
+            rospy.wait_for_message(self._topic_odom_initial,Odometry,30.0)
+            rospy.wait_for_message(self._topic_odom_backup,Odometry,30.0)
+        except rospy.ROSException as e:
+            rospy.logwarn('Message is not published:{}'.format(e))
+
+        self._srv_select_topic = rospy.ServiceProxy('~select_service_topic', MuxSelect)
+        self._srv_select_tf = rospy.ServiceProxy('~select_service_tf', MuxSelect)
 
         self.r = rostopic.ROSTopicHz(-1)
         rospy.Subscriber(self._topic_odom_initial, rospy.AnyMsg, self.r.callback_hz, callback_args=self._topic_odom_initial)
         self._flag_backup = False
 
+        rospy.loginfo('odometry_mux_selector is up')
+
     def select(self, topic_odom, topic_tf):
 
         try:
-            self._srv_topic(topic_odom)
-            self._srv_tf(topic_tf)
+            self._srv_select_topic(topic_odom)
+            self._srv_select_tf(topic_tf)
         except (rospy.ServiceException, rospy.ROSException) as e:
             rospy.logerr('Service call failed: {}'.format(e))
 
     def spin(self):
 
-        rate = rospy.sleep(1)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             rate.sleep()
-            if not self._flag_backup and \
-                    self.r.get_hz(self._topic_odom_initial)[0] < self._rate_threshold:
-                rospy.logwarn('Publish rate of initial topic is low. switched to backup topics')
-                self._flag_backup = True
-                self.select( self._topic_odom_backup, self._topic_tf_backup )
+            try:
+                rospy.wait_for_message(self._topic_odom_initial,Odometry,30.0)
+            except rospy.ROSException as e:
+                if not self._flag_backup:
+                    rospy.logwarn('Initial topic "{}" seems not to be published. switched to backup topics'.format(self._topic_odom_initial))
+                    self._flag_backup = True
+                    self.select( self._topic_odom_backup, self._topic_tf_backup )
 
 def main():
 
