b414f941174dc4c50a3a6bee78393fd54f8f49db
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-04-03 18:22:39 +0900

    add set -x in install scripts

--- jsk_fetch_robot/jsk_fetch_startup/config/install_upstart.sh
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_upstart.sh
@@ -5,7 +5,8 @@ jsk_fetch_startup=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`"/.. > /dev/null &&
 IFS=':' read -r -a prefix_paths <<< "$CMAKE_PREFIX_PATH"
 current_prefix_path="${prefix_paths[0]}"
 
-#
+set -x
+
 cd $jsk_fetch_startup/upstart_scripts
 for file in $(ls ./*.conf); do
     sudo cp $file /etc/init/
@@ -13,3 +14,5 @@ for file in $(ls ./*.conf); do
     sudo chmod 644 /etc/init/$file
     echo "copied $file to /etc/init"
 done
+
+set +x
