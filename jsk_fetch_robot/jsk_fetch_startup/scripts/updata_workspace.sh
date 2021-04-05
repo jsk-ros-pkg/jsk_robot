43be446a7da1aa782823cb3bfd921046e81fcf2e
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-04-06 00:14:58 +0900

    add foreach before wstool update

--- jsk_fetch_robot/jsk_fetch_startup/scripts/updata_workspace.sh
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/updata_workspace.sh
@@ -6,6 +6,7 @@ set -x
 
 cd $HOME/ros/melodic/src
 ln -sf $(rospack find jsk_fetch_startup)/../jsk_fetch.rosinstall.$ROS_DISTRO $HOME/ros/melodic/src/.rosinstall
+wstool foreach --git 'git stash'
 wstool update --delete-changed-uris --continue-on-error
 catkin build
 set +x
