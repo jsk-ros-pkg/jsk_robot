db9d33a70940f392af2bf0654c3126825e86769f
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-04-07 13:29:57 +0900

    build with release

--- jsk_fetch_robot/jsk_fetch_startup/scripts/updata_workspace.sh
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/updata_workspace.sh
@@ -8,5 +8,8 @@ cd $HOME/ros/melodic/src
 ln -sf $(rospack find jsk_fetch_startup)/../jsk_fetch.rosinstall.$ROS_DISTRO $HOME/ros/melodic/src/.rosinstall
 wstool foreach --git 'git stash'
 wstool update --delete-changed-uris
-catkin build
+cd $HOME/ros/melodic
+catkin init
+catkin config -DCMAKE_BUILD_TYPE=Release
+catkin build -DCMAKE_BUILD_TYPE=Release
 set +x
