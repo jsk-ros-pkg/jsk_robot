de62472bf8135cbb7a909d9d8a4d4081e0c29e03
Naoya Yamaguchi <708yamaguchi@gmail.com>
2021-04-21 17:12:05 +0900

    Add cron install scripts

---
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/update_workspace.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. $HOME/ros/melodic/devel/setup.bash
+
+set -x
+
+cd $HOME/ros/melodic/src
+ln -sf $(rospack find jsk_fetch_startup)/../jsk_fetch.rosinstall.$ROS_DISTRO $HOME/ros/melodic/src/.rosinstall
+wstool foreach --git 'git stash'
+wstool update --delete-changed-uris
+cd $HOME/ros/melodic
+catkin init
+catkin config -DCMAKE_BUILD_TYPE=Release
+catkin build
+set +x
