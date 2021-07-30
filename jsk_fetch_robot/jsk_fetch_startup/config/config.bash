59bf353770dbfdd5589ecd64b8b8b7a85b031375
Koki Shinjo <sktometometo@gmail.com>
2021-07-30 09:10:07 +0900

    Add Head L515 and update realsense launch files (#132)
    * [jsk_fetch_startup] add RS_SERIALNO_* args
    
    * [jsk_fetch_startup] add L515 at head
    
    * [jsk_fetch_startup] set serial number of realsense to be environment variables
    
    * [jsk_fetch_startup] change SERIALNO to SERIAL_NO
    
    * [jsk_fetch_startup] launch realsense only when serial number is specified
    
    * [jsk_fetch_startup] add config.bash for robot.conf and update robot.conf to source it if it exists in /var/lib/robot
    
    * [jsk_fetch_startup] update L515 config
    
    * [jsk_fetch_startup] update xacro for l515
    
    * [jsk_fetch_accessories] add head_l515
    
    * [jsk_fetch_startup] update head_l515 parameters
    
    * [jsk_fetch_startup] fix l515 urdf parameter
    
    * [jsk_fetch_startup] update rviz config for l515

---
+++ jsk_fetch_robot/jsk_fetch_startup/config/config.bash
@@ -0,0 +1,7 @@
+# This file is bash configuration for robot.conf
+# This file must be at /var/lib/robot/config.bash
+
+export RS_SERIAL_NO_T265=""
+export RS_SERIAL_NO_D435_FRONTRIGHT=""
+export RS_SERIAL_NO_D435_FRONTLEFT=""
+export RS_SERIAL_NO_L515_HEAD=""
