ad107e72932a126f041f82ac9056eab1b0bbafee
Koki Shinjo <sktometometo@gmail.com>
2021-08-13 11:30:38 +0900

    [jsk_spot_startup] update battery scripts and update launch

--- jsk_spot_robot/jsk_spot_startup/node_scripts/cable_connection_detector.py
+++ jsk_spot_robot/jsk_spot_startup/node_scripts/cable_connection_detector.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python
 
 import rospy
-from spot_msgs.msg import BatteryStateArray, BatteryState
+import spot_msgs.msg.BatteryStateArray, spot_msgs.msg.BatteryState
+import sensor_msgs.msg.BatteryState
 from std_msgs.msg import Bool
 
 class CalbeConnectionDetector:
 
     def __init__(self):
 
-        rospy.pub = rospy.Publisher('/spot/status/cable_connected',Bool,queue_size=1)
-        rospy.sub_battery_state = rospy.Subscriber('/spot/status/battery_states',BatteryStateArray,self.callback_battery_state)
+        self.pub = rospy.Publisher('/spot/status/cable_connected',Bool,queue_size=1)
 
-    def callback_battery_state(self,msg):
+        self.msg_battery_spot = None
+        self.msg_battery_laptop = None
 
-        if msg.battery_states[0].status == BatteryState.STATUS_DISCHARGING:
-            rospy.pub.publish(Bool(False))
-        else:
-            rospy.pub.publish(Bool(True))
+        self.sub_battery_spot = rospy.Subscriber(
+                                    '/spot/status/battery_states',
+                                    spot_msgs.msg.BatteryStateArray,
+                                    self.callback_battery_spot)
+        self.sub_battery_laptop = rospy.Subscriber(
+                                    '/laptop_charge',
+                                    sensor_msgs.msg.BatteryState,
+                                    self.callback_battery_laptop)
+
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            rate.sleep()
+            if self.msg_battery_spot == None or self.msg_battery_laptop == None:
+                continue
+            self.pub.publish(Bool(self.check_connection()))
+
+    def check_connection(self):
+
+        connected = False
+
+        if self.msg_battery_spot.battery_states[0].status == spot_msgs.msg.BatteryState.STATUS_CHARGING or\
+               self.msg_battery_laptop.power_supply_status == sensor_msgs.msg.BatteryState.POWER_SUPPLY_STATUS_CHARGING or\
+               self.msg_battery_laptop.power_supply_status == sensor_msgs.msg.BatteryState.POWER_SUPPLY_STATUS_NOT_CHARGING: #?
+            return True
+
+        return connected
+
+    def callback_battery_spot(self,msg):
+
+        self.msg_battery_spot = msg
+
+    def callback_battery_laptop(self,msg):
+
+        self.msg_battery_laptop = msg
+
 
 def main():
 
