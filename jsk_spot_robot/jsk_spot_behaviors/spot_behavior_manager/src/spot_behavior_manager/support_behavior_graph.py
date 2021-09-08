ae58c664cfaf2d3caae06c78d9161f29e2fd708c
Koki Shinjo <sktometometo@gmail.com>
2021-09-08 11:33:11 +0900

    [spot_behavior_manager] update to search new path when some edge failed

--- jsk_spot_robot/jsk_spot_behaviors/spot_behavior_manager/src/spot_behavior_manager/support_behavior_graph.py
+++ jsk_spot_robot/jsk_spot_behaviors/spot_behavior_manager/src/spot_behavior_manager/support_behavior_graph.py
@@ -26,31 +26,25 @@ class GraphNode:
 
 class SupportBehaviorGraph:
 
+    # 現在の SupportBehaviorGraph の仕様
+    #   重み付きの有向グラフ
+    #   あるノードからあるノードまでのエッジの数は 0 or 1
+
     def __init__(self, raw_edges=[], raw_nodes={}):
 
         self.edges = {}
         self.nodes = {}
         self.network = nx.DiGraph()
 
-        edges = []
         for raw_edge in raw_edges:
-            edges.append( GraphEdge( raw_edge['from'],
-                                     raw_edge['to'],
-                                     raw_edge['behavior_type'],
-                                     int(raw_edge['cost']),
-                                     raw_edge['args'] ))
-        nodes = {}
-        for key, raw_node in raw_nodes.items():
-            nodes[key] = GraphNode( key, raw_node )
+            self.add_edge( raw_edge['from'],
+                           raw_edge['to'],
+                           raw_edge['behavior_type'],
+                           int(raw_edge['cost']),
+                           raw_edge['args'] )
 
-        for key, node in nodes.items():
-            self.nodes[key] = node
-        for edge in edges:
-            self.edges[edge.node_id_from,edge.node_id_to] = edge
-            self.network.add_edge(
-                                edge.node_id_from,
-                                edge.node_id_to,
-                                weight=edge.cost)
+        for key, raw_node in raw_nodes.items():
+            self.add_node( key, raw_node )
 
     def calcPath(self, node_id_from, node_id_to):
 
@@ -62,3 +56,38 @@ class SupportBehaviorGraph:
         for index in range(len(node_id_list)-1):
             path.append(self.edges[node_id_list[index],node_id_list[index+1]])
         return path
+
+    def add_node( self,
+                  node_id,
+                  properties ):
+        # TODO: もしすでにnodeがあれば,適切に上書きするようにする
+        # NOTICE: node と edge で整合性を保つようには実装されていない
+        self.nodes[node_id] = GraphNode( node_id, properties )
+
+    def add_edge( self,
+                  node_id_from,
+                  node_id_to,
+                  behavior_type,
+                  cost,
+                  args ):
+        # TODO: もしすでにedgeがあれば,適切に上書きするようにする
+        # NOTICE: node と edge で整合性を保つようには実装されていない
+        edge = GraphEdge( node_id_from,
+                          node_id_to,
+                          behavior_type,
+                          cost,
+                          args )
+        self.edges[ node_id_from, node_id_to ] = edge
+        self.network.add_edge( node_id_from,
+                               node_id_to,
+                               weight=cost )
+
+    def remove_node( self,
+                     node_id ):
+        del self.nodes[node_id]
+
+    def remove_edge( self,
+                     node_id_from,
+                     node_it_to ):
+        del self.edges[ node_id_from, node_id_to ]
+        self.network.remove_edge( node_id_from, node_id_to )
