ddf0ec8af2c5bbdc666f4bda075ceeaa9d3e6798
Koki Shinjo <sktometometo@gmail.com>
2021-09-08 12:52:57 +0900

    [spot_basic_behaviors] update format

--- jsk_spot_robot/jsk_spot_behaviors/spot_basic_behaviors/node_scripts/car_tracker.py
+++ jsk_spot_robot/jsk_spot_behaviors/spot_basic_behaviors/node_scripts/car_tracker.py
@@ -13,8 +13,10 @@ from std_msgs.msg import Bool
 from jsk_recognition_msgs.msg import BoundingBox, BoundingBoxArray
 from jsk_recognition_msgs.msg import Label, LabelArray
 
+
 def convert_msg_point_to_kdl_vector(point):
-    return PyKDL.Vector(point.x,point.y,point.z)
+    return PyKDL.Vector(point.x, point.y, point.z)
+
 
 class TrackedObject(object):
 
@@ -56,7 +58,8 @@ class TrackedObject(object):
 
         with self.lock:
             if observed_time < self.last_observed:
-                rospy.logerr('observed time is earlier than last observed time, dropped.')
+                rospy.logerr(
+                    'observed time is earlier than last observed time, dropped.')
                 return
             elif observed_time == self.last_observed:
                 rospy.loginfo('this object is already observed')
@@ -64,7 +67,8 @@ class TrackedObject(object):
 
             # Update position and velocity
             if self.state == self.FOUND or self.state == self.MOVING or self.state == self.APPROCHING:
-                self.velocity = (observed_position - self.position) / (observed_time - self.last_observed).to_sec()
+                self.velocity = (observed_position - self.position) / \
+                    (observed_time - self.last_observed).to_sec()
             else:
                 self.velocity = PyKDL.Vector()
             self.position = observed_position
@@ -73,7 +77,7 @@ class TrackedObject(object):
             # Update State
             if (self.state == self.LOST or self.state == self.UNKNOWN) or \
                 (self.velocity.Norm() < threshold_velocity) or \
-                ((robot_position_fixedbased - self.position).Norm() > threshold_distance):
+                    ((robot_position_fixedbased - self.position).Norm() > threshold_distance):
                 self.state = self.FOUND
             else:
                 self.state = self.MOVING
@@ -81,21 +85,23 @@ class TrackedObject(object):
     def getDistance(self, robot_position):
         return (self.position - robot_position).Norm()
 
+
 class MultiObjectTracker(object):
 
     def __init__(self):
 
         self._frame_fixed = rospy.get_param('~frame_fixed', 'fixed_frame')
         self._frame_robot = rospy.get_param('~frame_robot', 'base_link')
-        self._timeout_transform = rospy.get_param('~timeout_transform',0.05)
+        self._timeout_transform = rospy.get_param('~timeout_transform', 0.05)
         slop = rospy.get_param('~slop', 0.1)
 
         # parameters for multi object trackers
-        ## maximum number of tracking objects
+        # maximum number of tracking objects
         self._num_max_track = rospy.get_param('~num_max_track', 10)
-        ## threshold for MOT state update
+        # threshold for MOT state update
         self._thresholds_distance = rospy.get_param('~thresholds_distance', {})
-        self._threshold_velocity = rospy.get_param('~threshold_move_velocity', 1.0)
+        self._threshold_velocity = rospy.get_param(
+            '~threshold_move_velocity', 1.0)
         self._threshold_lost = rospy.get_param('~threshold_lost_duration', 1.0)
 
         # members
@@ -107,27 +113,31 @@ class MultiObjectTracker(object):
         self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
 
         # Publisher
-        ## publsher for if there is a moving object or not
+        # publsher for if there is a moving object or not
         self._pub_visible = rospy.Publisher(
-                '~visible',
-                Bool,
-                queue_size=1
-                )
+            '~visible',
+            Bool,
+            queue_size=1
+        )
 
         # Subscriber
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
             try:
-                rospy.wait_for_message('~input_bbox_array', BoundingBoxArray, 3)
+                rospy.wait_for_message(
+                    '~input_bbox_array', BoundingBoxArray, 3)
                 rospy.wait_for_message('~input_tracking_labels', LabelArray, 3)
                 break
             except (rospy.ROSException, rospy.ROSInterruptException) as e:
                 rospy.logerr('subscribing topic seems not to be published.')
                 rospy.logerr('Error: {}'.format(e))
             rate.sleep()
-        mf_sub_bbbox_array = message_filters.Subscriber('~input_bbox_array', BoundingBoxArray)
-        mf_sub_tracking_labels = message_filters.Subscriber('~input_tracking_labels', LabelArray)
-        ts = message_filters.ApproximateTimeSynchronizer([mf_sub_bbbox_array, mf_sub_tracking_labels], 10, slop)
+        mf_sub_bbbox_array = message_filters.Subscriber(
+            '~input_bbox_array', BoundingBoxArray)
+        mf_sub_tracking_labels = message_filters.Subscriber(
+            '~input_tracking_labels', LabelArray)
+        ts = message_filters.ApproximateTimeSynchronizer(
+            [mf_sub_bbbox_array, mf_sub_tracking_labels], 10, slop)
         ts.registerCallback(self._cb_object)
 
     def _cb_object(self,
@@ -167,26 +177,28 @@ class MultiObjectTracker(object):
                         self._dict_objects[tracking_label.id] = \
                             TrackedObject(
                                 bbox.label,
-                                convert_msg_point_to_kdl_vector(bbox.pose.position),
+                                convert_msg_point_to_kdl_vector(
+                                    bbox.pose.position),
                                 time_observed
-                                )
+                        )
                     else:
-                        rospy.logwarn('number of objects exceeds max track. dropped.')
+                        rospy.logwarn(
+                            'number of objects exceeds max track. dropped.')
                 else:
                     self._dict_objects[tracking_label.id].update(
-                                convert_msg_point_to_kdl_vector(bbox.pose.position),
-                                time_observed,
-                                pykdl_transform_fixed_to_robot.p,
-                                self._threshold_velocity,
-                                self._thresholds_distance[str(bbox.label)]
-                                )
+                        convert_msg_point_to_kdl_vector(bbox.pose.position),
+                        time_observed,
+                        pykdl_transform_fixed_to_robot.p,
+                        self._threshold_velocity,
+                        self._thresholds_distance[str(bbox.label)]
+                    )
             # check if there is lost object
             to_be_removed = []
             for key in self._dict_objects:
                 is_lost = self._dict_objects[key].checkLost(
-                                time_observed,
-                                self._threshold_lost
-                                )
+                    time_observed,
+                    self._threshold_lost
+                )
                 if is_lost:
                     to_be_removed.append(key)
             # remove lost object from dict
@@ -209,11 +221,13 @@ class MultiObjectTracker(object):
 
                 self._pub_visible.publish(Bool(exist_moving_object))
 
+
 def main():
 
     rospy.init_node('person_tracker')
     tracker = MultiObjectTracker()
     tracker.spin()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
