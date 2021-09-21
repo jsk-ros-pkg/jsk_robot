5e85084c726e7bc9ac843c8ad41c5eacc66e1afb
Naoya Yamaguchi <708yamaguchi@gmail.com>
2021-09-21 18:57:13 +0900

    Remove unnecessary variables

--- jsk_fetch_robot/jsk_fetch_startup/config/install_udev.sh
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_udev.sh
@@ -2,10 +2,6 @@
 
 jsk_fetch_startup=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`"/.. > /dev/null && pwd)
 
-IFS=':' read -r -a prefix_paths <<< "$CMAKE_PREFIX_PATH"
-current_prefix_path="${prefix_paths[0]}"
-
-#
 cd $jsk_fetch_startup/udev_rules
 for file in $(ls ./*.rules); do
     sudo cp $file /etc/udev/rules.d/
