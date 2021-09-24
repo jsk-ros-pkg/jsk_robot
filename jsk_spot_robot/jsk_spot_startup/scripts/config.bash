dae365625ad706c09a047631c05ed6a5bd727b8b
Koki Shinjo <sktometometo@gmail.com>
2021-09-24 19:12:03 +0900

    [jsk_spot_startup] update config

--- jsk_spot_robot/jsk_spot_startup/scripts/config.bash
+++ jsk_spot_robot/jsk_spot_startup/scripts/config.bash
@@ -2,14 +2,17 @@
 # This file must be at /var/lib/robot/config.bash
 
 # Network interfaces
-IF_ETH="enx70886b8f1b38"
-IF_WIFI="wlxd037458e7f3c"
-IF_LTE="enxf8b7975c750a"
+export IF_ETH="enx70886b8f1b38"
+export IF_WIFI="wlxd037458e7f3c"
+export IF_LTE="enxf8b7975c750a"
 
 # ROS_IP and ROS_MASTER_URI
 WIFI_AP_IP=10.42.0.1
 rossetmaster $WIFI_AP_IP
 rossetip $WIFI_AP_IP
+
+# ROS CONSOLE
+export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
 
 # Credentials
 # export GOOGLE_APPLICATION_CREDENTIALS=/path/to/service_account_json_file
