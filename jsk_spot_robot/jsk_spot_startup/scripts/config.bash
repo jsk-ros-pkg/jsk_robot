7ae0a4fe6282dc32c00e538e99fe423ed466ed00
Koki Shinjo <sktometometo@gmail.com>
2021-09-24 18:13:47 +0900

    [jsk_spot_startup] update service, scripts

--- jsk_spot_robot/jsk_spot_startup/scripts/config.bash
+++ jsk_spot_robot/jsk_spot_startup/scripts/config.bash
@@ -1,2 +1,17 @@
 # This file is robot-specific bash configuration used by robot systemd servies
 # This file must be at /var/lib/robot/config.bash
+
+# Network interfaces
+IF_ETH="enx70886b8f1b38"
+IF_WIFI="wlxd037458e7f3c"
+IF_LTE="enxf8b7975c750a"
+
+# ROS_IP and ROS_MASTER_URI
+WIFI_AP_IP=10.42.0.1
+rossetmaster $WIFI_AP_IP
+rossetip $WIFI_AP_IP
+
+# Credentials
+# export GOOGLE_APPLICATION_CREDENTIALS=/path/to/service_account_json_file
+# export DIALOGFLOW_PROJECT_ID=<your dialogflow project id>
+# export GOOGLE_DRIVE_SETTINGS_YAML=/path/to/pyDrive_setting_yaml
