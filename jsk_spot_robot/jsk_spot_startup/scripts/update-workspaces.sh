1823fcc293407d40cbda78769ec7d4ee4ec3df91
Koki Shinjo <sktometometo@gmail.com>
2021-10-02 11:38:18 +0900

    [jsk_spot_robot] update workspace updating script

--- jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
@@ -4,17 +4,29 @@ WORKSPACE_SPOT_DRIVER=/home/spot/spot_driver_ws
 WORKSPACE_SPOT_CORAL=/home/spot/spot_coral_ws
 WORKSPACE_SPOT=/home/spot/spot_ws
 
-function update_workspace() {
-    workspace=$1
-    if [ -e $workspace ]; then
-        cd $workspace/src
-        wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-        catkin build -j4 --continue-on-failure
-    else
-        echo "Workspace $workspace not found."
-    fi
-}
+if [ -e $WORKSPACE_SPOT_DRIVER ]; then
+    cd $WORKSPACE_SPOT_DRIVER/src
+    wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_startup/jsk_spot_driver.rosinstall -y -r
+    wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
+    catkin build -j4 --continue-on-failure
+else
+    echo "Workspace $WORKSPACE_SPOT_DRIVER not found."
+fi
 
-update_workspace $WORKSPACE_SPOT_DRIVER
-update_workspace $WORKSPACE_SPOT_CORAL
-update_workspace $WORKSPACE_SPOT
+if [ -e $WORKSPACE_SPOT_CORAL ]; then
+    cd $WORKSPACE_SPOT_CORAL/src
+    wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_startup/jsk_spot_coral.rosinstall -y -r
+    wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
+    catkin build -j4 --continue-on-failure
+else
+    echo "Workspace $WORKSPACE_SPOT_CORAL not found."
+fi
+
+if [ -e $WORKSPACE_SPOT ]; then
+    cd $WORKSPACE_SPOT/src
+    wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_startup/jsk_spot_user.rosinstall -y -r
+    wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
+    catkin build -j4 --continue-on-failure
+else
+    echo "Workspace $WORKSPACE_SPOT not found."
+fi
