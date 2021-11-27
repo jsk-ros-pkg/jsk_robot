090a784aad4e81d978effafd8193d373c90f5199
Naoto Tsukamoto <naototukka0413@gmail.com>
2021-11-27 13:30:37 +0900

    [jsk_spot_startup] move power-on in if clause

--- jsk_spot_robot/jsk_spot_startup/scripts/auto-power-on.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/auto-power-on.sh
@@ -2,9 +2,10 @@
 
 # claim, power-on and stand
 rosservice call /spot/claim "{}"
-rosservice call /spot/power_on "{}"
+
 # depend on docking station
 if ! "${IS_DOCK}"; then
+  rosservice call /spot/power_on "{}"
   rosservice call /spot/stand "{}"
 else
   echo "not standing because of dock" 1>&2
