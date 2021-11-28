f1cb6affffff8874a13e595d206c2316d2177105
Koki Shinjo <sktometometo@gmail.com>
2021-11-28 17:45:04 +0900

    [jsk_spot_startup] fix auto-power-on.sh

--- jsk_spot_robot/jsk_spot_startup/scripts/auto-power-on.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/auto-power-on.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# claim, power-on and stand
+# claim, power-on
 rosservice call /spot/claim "{}"
+rosservice call /spot/power_on "{}"
 
-# depend on docking station
-if ! "${IS_DOCK}"; then
-  rosservice call /spot/power_on "{}"
+# stand when not connected to a docking station
+if [[ -z ${STAND_AFTER_AUTO_POWER_ON} ]]; then
+    echo "please set STAND_AFTER_AUTO_POWER_ON variable"
+    exit 1
+elif ! "${STAND_AFTER_AUTO_POWER_ON}"; then
   rosservice call /spot/stand "{}"
 else
   echo "not standing because of dock" 1>&2
