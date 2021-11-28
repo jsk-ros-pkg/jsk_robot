e4bd949c856c379f240a77ae19fe3bc63c37a322
Koki Shinjo <sktometometo@gmail.com>
2021-11-28 17:50:47 +0900

    [jsk_spot_startup] fix print message and format

--- jsk_spot_robot/jsk_spot_startup/scripts/auto-power-on.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/auto-power-on.sh
@@ -6,10 +6,10 @@ rosservice call /spot/power_on "{}"
 
 # stand when not connected to a docking station
 if [[ -z ${STAND_AFTER_AUTO_POWER_ON} ]]; then
-    echo "please set STAND_AFTER_AUTO_POWER_ON variable"
-    exit 1
+  echo "please set STAND_AFTER_AUTO_POWER_ON variable"
+  exit 1
 elif ! "${STAND_AFTER_AUTO_POWER_ON}"; then
   rosservice call /spot/stand "{}"
 else
-  echo "not standing because of dock" 1>&2
+  echo "Do not stand because STAND_AFTER_AUTO_POWER_ON is set to false" 1>&2
 fi
