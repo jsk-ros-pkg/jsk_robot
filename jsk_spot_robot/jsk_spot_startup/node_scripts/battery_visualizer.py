07741f642c71bd1f628b929def95cf9861695648
Koki Shinjo <sktometometo@gmail.com>
2021-12-03 19:29:07 +0900

    [jsk_spot_startup] fix format for python

--- jsk_spot_robot/jsk_spot_startup/node_scripts/battery_visualizer.py
+++ jsk_spot_robot/jsk_spot_startup/node_scripts/battery_visualizer.py
@@ -4,14 +4,17 @@ import rospy
 from spot_msgs.msg import BatteryStateArray
 from std_msgs.msg import Float32
 
+
 class SpotBatteryVisualizer(object):
 
     def __init__(self):
 
-        rospy.init_node( 'spot_battery_visualizer' )
+        rospy.init_node('spot_battery_visualizer')
 
-        self._sub = rospy.Subscriber( '/spot/status/battery_states', BatteryStateArray, self._cb )
-        self._pub = rospy.Publisher( '/spot/status/battery_percentage', Float32, queue_size=1 )
+        self._sub = rospy.Subscriber(
+            '/spot/status/battery_states', BatteryStateArray, self._cb)
+        self._pub = rospy.Publisher(
+            '/spot/status/battery_percentage', Float32, queue_size=1)
 
     def _cb(self, msg):
 
@@ -19,10 +22,12 @@ class SpotBatteryVisualizer(object):
         pub_msg.data = msg.battery_states[0].charge_percentage
         self._pub.publish(pub_msg)
 
+
 def main():
 
     battery_visualizer = SpotBatteryVisualizer()
     rospy.spin()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- jsk_spot_robot/jsk_spot_startup/node_scripts/cable_connection_detector.py
+++ jsk_spot_robot/jsk_spot_startup/node_scripts/cable_connection_detector.py
@@ -5,23 +5,25 @@ import spot_msgs.msg
 import sensor_msgs.msg
 from std_msgs.msg import Bool
 
+
 class CalbeConnectionDetector:
 
     def __init__(self):
 
-        self.pub = rospy.Publisher('/spot/status/cable_connected',Bool,queue_size=1)
+        self.pub = rospy.Publisher(
+            '/spot/status/cable_connected', Bool, queue_size=1)
 
         self.msg_battery_spot = None
         self.msg_battery_laptop = None
 
         self.sub_battery_spot = rospy.Subscriber(
-                                    '/spot/status/power_state',
-                                    spot_msgs.msg.PowerState,
-                                    self.callback_battery_spot)
+            '/spot/status/power_state',
+            spot_msgs.msg.PowerState,
+            self.callback_battery_spot)
         self.sub_battery_laptop = rospy.Subscriber(
-                                    '/laptop_charge',
-                                    sensor_msgs.msg.BatteryState,
-                                    self.callback_battery_laptop)
+            '/laptop_charge',
+            sensor_msgs.msg.BatteryState,
+            self.callback_battery_laptop)
 
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
@@ -33,20 +35,20 @@ class CalbeConnectionDetector:
     def check_connection(self):
 
         if self.msg_battery_spot is not None and\
-             self.msg_battery_spot.shore_power_state == spot_msgs.msg.PowerState.STATE_ON_SHORE_POWER:
+                self.msg_battery_spot.shore_power_state == spot_msgs.msg.PowerState.STATE_ON_SHORE_POWER:
             return True
         elif self.msg_battery_laptop is not None and\
-             (self.msg_battery_laptop.power_supply_status == sensor_msgs.msg.BatteryState.POWER_SUPPLY_STATUS_CHARGING or\
-              self.msg_battery_laptop.power_supply_status == sensor_msgs.msg.BatteryState.POWER_SUPPLY_STATUS_NOT_CHARGING):
+            (self.msg_battery_laptop.power_supply_status == sensor_msgs.msg.BatteryState.POWER_SUPPLY_STATUS_CHARGING or
+             self.msg_battery_laptop.power_supply_status == sensor_msgs.msg.BatteryState.POWER_SUPPLY_STATUS_NOT_CHARGING):
             return True
         else:
             return False
 
-    def callback_battery_spot(self,msg):
+    def callback_battery_spot(self, msg):
 
         self.msg_battery_spot = msg
 
-    def callback_battery_laptop(self,msg):
+    def callback_battery_laptop(self, msg):
 
         self.msg_battery_laptop = msg
 
