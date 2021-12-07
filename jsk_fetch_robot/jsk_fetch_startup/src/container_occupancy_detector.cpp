e4ff7388117c166bef07727820453b418014bb5c
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2021-12-07 17:30:13 +0900

    change output message type, use boundingboxarray

--- jsk_fetch_robot/jsk_fetch_startup/src/container_occupancy_detector.cpp
+++ jsk_fetch_robot/jsk_fetch_startup/src/container_occupancy_detector.cpp
@@ -9,7 +9,6 @@
 
 #include <ros/ros.h>
 #include <ros/console.h>
-#include <std_msgs/Float32MultiArray.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 #include <geometry_msgs/Point.h>
@@ -45,13 +44,12 @@ public:
     this->_containerBoxesSub = _nh.subscribe("trashbin_occupancy_detector/trashbin/boxes", 10, &ContainerOccupancyDetector::CalculateOccupancyCB, this);
     this->_pointCloudSub = _nh.subscribe("head_camera/depth_registered/points", 10, &ContainerOccupancyDetector::PointCloudTransformCB, this); // TODO: rename topic name
     this->_debugPointPub = _nh.advertise<sensor_msgs::PointCloud2>("debug_pointcloud", 10);
-    this->_occupancyPub = _nh.advertise<std_msgs::Float32MultiArray>("container_occupancy", 10);
+    this->_occupancyPub = _nh.advertise<jsk_recognition_msgs::BoundingBoxArray>("container_occupancy", 10);
     // init default orientation const
     this->defaultOrientation.w = 1.0;
     this->defaultOrientation.x = 0.0;
     this->defaultOrientation.y = 0.0;
-    this->defaultOrientation.z = 0.0;
-    
+    this->defaultOrientation.z = 0.0;    
   };
   // destructor
   ~ContainerOccupancyDetector(){
@@ -75,41 +73,45 @@ public:
     const sensor_msgs::PointCloud2 inputCloud = _transformedCloudMsg;
     pcl::PCLPointCloud2 pclPC2;
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclCloud(new pcl::PointCloud<pcl::PointXYZ>);
-    std_msgs::Float32MultiArray occupancy;
-    occupancy.data.resize(msg.boxes.size());
+    jsk_recognition_msgs::BoundingBoxArray boxOccupancy = msg;
     if (!inputCloud.data.empty()){
       // convert to PCL cloud
       pcl_conversions::toPCL(inputCloud, pclPC2);
       pcl::fromPCLPointCloud2(pclPC2, *pclCloud);
-      for (int i=0; i<msg.boxes.size(); i++){
+      for (int i = 0; i < msg.boxes.size(); i++) {
         // calculate each bounding boxes
         // pcl::PassThrough<pcl::PointXYZ> filter;
         pcl::CropBox<pcl::PointXYZ> boxFilter;
-        pcl::PointCloud<pcl::PointXYZ>::Ptr boxFilteredCloud (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr boxFilteredCloud(
+            new pcl::PointCloud<pcl::PointXYZ>);
         pcl::PointXYZ minPt, maxPt;
-        boxFilter.setMin(Eigen::Vector4f(
-                           (msg.boxes.at(i).pose.position.x - msg.boxes.at(i).dimensions.x / 2.0),
-                           (msg.boxes.at(i).pose.position.y - msg.boxes.at(i).dimensions.y / 2.0),
-                           FLOAT_MIN,
-                           1.0f));
-        boxFilter.setMax(Eigen::Vector4f(
-                           (msg.boxes.at(i).pose.position.x + msg.boxes.at(i).dimensions.x / 2.0),
-                           (msg.boxes.at(i).pose.position.y + msg.boxes.at(i).dimensions.y / 2.0),
-                           FLOAT_MAX,
-                           1.0f));
+        float occupancy;
+        boxFilter.setMin(Eigen::Vector4f((msg.boxes.at(i).pose.position.x -
+                                          msg.boxes.at(i).dimensions.x / 2.0),
+                                         (msg.boxes.at(i).pose.position.y -
+                                          msg.boxes.at(i).dimensions.y / 2.0),
+                                         FLOAT_MIN, 1.0f));
+        boxFilter.setMax(Eigen::Vector4f((msg.boxes.at(i).pose.position.x +
+                                          msg.boxes.at(i).dimensions.x / 2.0),
+                                         (msg.boxes.at(i).pose.position.y +
+                                          msg.boxes.at(i).dimensions.y / 2.0),
+                                         FLOAT_MAX, 1.0f));
         boxFilter.setInputCloud(pclCloud);
         boxFilter.filter(*boxFilteredCloud);
-        pcl::getMinMax3D (*boxFilteredCloud, minPt, maxPt);
-        ROS_INFO_STREAM("maxPt.z: " << maxPt.z << std::endl);
-        if(maxPt.z > msg.boxes.at(i).pose.position.z + msg.boxes.at(i).dimensions.z / 2.0){
-          // if some point clouds in boundingbox over bounding height
-          // TODO publish occupancy instead
-          occupancy.data.push_back(1.0);
-        } else {
-          occupancy.data.push_back(0.0);
-        }
+        pcl::getMinMax3D(*boxFilteredCloud, minPt, maxPt);
+        ROS_INFO_STREAM(
+            "box_id: " << i << " box_pos: " << msg.boxes.at(i).pose.position
+                       << " box_height: "
+                       << msg.boxes.at(i).pose.position.z +
+                              msg.boxes.at(i).dimensions.z / 2.0
+                       << " maxPt.z: " << maxPt.z); // TODO Move to debug stream
+        occupancy = maxPt.z / (msg.boxes.at(i).pose.position.z +
+                               msg.boxes.at(i).dimensions.z / 2.0);
+        // if some point clouds in boundingbox over bounding height
+        // TODO publish occupancy instead
+        boxOccupancy.boxes.at(i).value = occupancy;
       }
-      this->_occupancyPub.publish(occupancy);
+      this->_occupancyPub.publish(boxOccupancy);
     } else {
       ROS_WARN("No input point clouds.");
     }
