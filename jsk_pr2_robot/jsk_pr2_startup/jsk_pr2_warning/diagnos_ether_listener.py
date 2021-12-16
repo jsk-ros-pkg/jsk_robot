35ae0962b44304c5fecb8afad27c3f2edf851cee
Kanazawanaoaki <kanazawa@jsk.imi.i.u-tokyo.ac.jp>
2021-12-16 11:49:57 +0900

    add diagnos_ether_listener.py

---
+++ jsk_pr2_robot/jsk_pr2_startup/jsk_pr2_warning/diagnos_ether_listener.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+from diagnostic_msgs.msg import DiagnosticArray
+
+hoge = None
+
+def callback(data):
+    global hoge
+    flag = False
+    for e in data.status:
+        #EtherCAT Device
+        for v in e.values:
+            if v.key =='Drops':
+                flag = True
+                print(e.name + "Drops: {}".format(v.value))
+        #EtherCAT Master
+        if e.name == 'EtherCAT Master':
+            for v in e.values:
+                if v.key =='Dropped Packets':
+                    print(e.name + " Dropped Packets: {}".format(v.value))
+                if v.key =='RX Late Packet':
+                    print(e.name + " RX Late Packet: {}".format(v.value))                    
+
+    if flag:
+        print("--------------------------------------------")
+        
+    
+def listener():
+    rospy.init_node('diagnostics_listener', anonymous=True)
+    rospy.Subscriber('/diagnostics', DiagnosticArray, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
