7266ebd5dae2c6def3aa36aecb282f2853f406b8
Koki Shinjo <sktometometo@gmail.com>
2021-12-24 21:46:16 +0900

    [jsk_spot_startup] update script

--- jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
@@ -7,60 +7,47 @@ MAIL_BODY_FILE=/tmp/mail-body.txt
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-echo "Result of workspace updating\n" > $MAIL_BODY_FILE
+echo "Result of workspace updating in $ROBOT_NAME\n" > $MAIL_BODY_FILE
 
-if [ -e $WORKSPACE_SPOT_DRIVER ]; then
-    cd $WORKSPACE_SPOT_DRIVER/src
-    wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_driver.rosinstall --merge-replace --confirm-all
-    wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-    catkin build --no-status -j4 --continue-on-failure 2>&1 | tee /tmp/build_spot_driver.log
-    if [ $? -eq 0 ]; then
-        echo "Updating and building of $WORKSPACE_SPOT_DRIVER succeeded.\n" >> $MAIL_BODY_FILE
-        echo "Updating and building of $WORKSPACE_SPOT_DRIVER succeeded.\n"
+function update_and_build () {
+    local WORKSPACE=$1
+    local UPDATE_LOG=$2
+    local BUILD_LOG=$3
+    if [ -e $WORKSPACE ]; then
+        cd $WORKSPACE/src
+        wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_driver.rosinstall --merge-replace --confirm-all
+        wstool update -t . -j 4 -m 60 --delete-changed-uris 2>&1 | tee $UPDATE_LOG
+        if [ $? -eq 0 ]; then
+            echo "Updating of $WORKSPACE succeeded.\n" >> $MAIL_BODY_FILE
+            echo "Updating of $WORKSPACE succeeded.\n"
+        else
+            echo "Updating of $WORKSPACE failed.\n" >> $MAIL_BODY_FILE
+            echo "Updating of $WORKSPACE failed.\n"
+        catkin build --no-status -j4 --continue-on-failure 2>&1 | tee $BUILD_LOG
+        if [ $? -eq 0 ]; then
+            echo "Building of $WORKSPACE succeeded.\n" >> $MAIL_BODY_FILE
+            echo "Building of $WORKSPACE succeeded.\n"
+        else
+            echo "Building of $WORKSPACE failed.\n" >> $MAIL_BODY_FILE
+            echo "Building of $WORKSPACE failed.\n"
+        fi
     else
-        echo "Updating and building of $WORKSPACE_SPOT_DRIVER failed.\n" >> $MAIL_BODY_FILE
-        echo "Updating and building of $WORKSPACE_SPOT_DRIVER failed.\n"
+        echo "Workspace $WORKSPACE not found.\n" >> $MAIL_BODY_FILE
+        echo "Workspace $WORKSPACE not found.\n"
+        touch $UPDATE_LOG
+        touch $BUILD_LOG
     fi
-else
-    echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n" >> $MAIL_BODY_FILE
-    echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n"
-    echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n" > /tmp/build_spot_driver.log
-fi
+}
 
-if [ -e $WORKSPACE_SPOT_CORAL ]; then
-    cd $WORKSPACE_SPOT_CORAL/src
-    wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_coral.rosinstall --merge-replace --confirm-all
-    wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-    catkin build --no-status -j4 --continue-on-failure 2>&1 | tee /tmp/build_spot_coral.log
-    if [ $? -eq 0 ]; then
-        echo "Updating and building of $WORKSPACE_SPOT_CORAL succeeded.\n" >> $MAIL_BODY_FILE
-        echo "Updating and building of $WORKSPACE_SPOT_CORAL succeeded.\n"
-    else
-        echo "Updating and building of $WORKSPACE_SPOT_CORAL failed.\n" >> $MAIL_BODY_FILE
-        echo "Updating and building of $WORKSPACE_SPOT_CORAL failed.\n"
-    fi
-else
-    echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n" >> $MAIL_BODY_FILE
-    echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n"
-    echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n" > /tmp/build_spot_coral.log
-fi
+update_and_build $WORKSPACE_SPOT_DRIVER /tmp/update_spot_driver.log /tmp/build_spot_driver.log
+update_and_build $WORKSPACE_SPOT_CORAL /tmp/update_spot_coral.log /tmp/build_spot_coral.log
+update_and_build $WORKSPACE_SPOT /tmp/update_spot.log /tmp/build_spot.log
 
-if [ -e $WORKSPACE_SPOT ]; then
-    cd $WORKSPACE_SPOT/src
-    wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_dev.rosinstall --merge-replace --confirm-all
-    wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-    catkin build --no-status -j4 --continue-on-failure 2>&1 | tee /tmp/build_spot.log
-    if [ $? -eq 0 ]; then
-        echo "Updating and building of $WORKSPACE_SPOT succeeded.\n" >> $MAIL_BODY_FILE
-        echo "Updating and building of $WORKSPACE_SPOT succeeded.\n"
-    else
-        echo "Updating and building of $WORKSPACE_SPOT failed.\n" >> $MAIL_BODY_FILE
-        echo "Updating and building of $WORKSPACE_SPOT failed.\n"
-    fi
-else
-    echo "Workspace $WORKSPACE_SPOT not found.\n" >> $MAIL_BODY_FILE
-    echo "Workspace $WORKSPACE_SPOT not found.\n"
-    echo "Workspace $WORKSPACE_SPOT not found.\n" > /tmp/build_spot.log
-fi
-
-cat $MAIL_BODY_FILE | mail -A /tmp/build_spot_driver.log -A /tmp/build_spot_coral.log -A /tmp/build_spot.log -s "Workspace Updating Notification from $ROBOT_NAME" -r spot-jsk@jsk.imi.i.u-tokyo.ac.jp spot@jsk.imi.i.u-tokyo.ac.jp
+cat $MAIL_BODY_FILE | mailx \
+    -A /tmp/update_spot_driver.log \
+    -A /tmp/update_spot_coral.log \
+    -A /tmp/update_spot.log \
+    -A /tmp/build_spot_driver.log \
+    -A /tmp/build_spot_coral.log \
+    -A /tmp/build_spot.log \
+    -s "Workspace Updating Notification from $ROBOT_NAME" -r spot-jsk@jsk.imi.i.u-tokyo.ac.jp spot@jsk.imi.i.u-tokyo.ac.jp
