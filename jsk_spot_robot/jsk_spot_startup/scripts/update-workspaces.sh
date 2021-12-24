5d2f7bf08f1202f0d5c7fca7a36da647f0f022e8
Koki Shinjo <sktometometo@gmail.com>
2021-12-24 21:53:05 +0900

    [jsk_spot_startup] update script

--- jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
@@ -44,11 +44,15 @@ update_and_build $WORKSPACE_SPOT_DRIVER /tmp/update_spot_driver.log /tmp/build_s
 update_and_build $WORKSPACE_SPOT_CORAL /tmp/update_spot_coral.log /tmp/build_spot_coral.log
 update_and_build $WORKSPACE_SPOT /tmp/update_spot.log /tmp/build_spot.log
 
-cat $MAIL_BODY_FILE | mailx \
-    -A /tmp/update_spot_driver.log \
-    -A /tmp/update_spot_coral.log \
-    -A /tmp/update_spot.log \
-    -A /tmp/build_spot_driver.log \
-    -A /tmp/build_spot_coral.log \
-    -A /tmp/build_spot.log \
-    -s "Workspace Updating Notification from $ROBOT_NAME" -r spot-jsk@jsk.imi.i.u-tokyo.ac.jp spot@jsk.imi.i.u-tokyo.ac.jp
+(uuencode \
+    /tmp/update_spot_driver.log \
+    /tmp/update_spot_coral.log \
+    /tmp/update_spot.log \
+    /tmp/build_spot_driver.log \
+    /tmp/build_spot_coral.log \
+    /tmp/build_spot.log ; \
+    cat $MAIL_BODY_FILE ) | \
+        mailx \
+        -s "Workspace Updating Notification from $ROBOT_NAME" \
+        -r spot-jsk@jsk.imi.i.u-tokyo.ac.jp \
+        spot@jsk.imi.i.u-tokyo.ac.jp
