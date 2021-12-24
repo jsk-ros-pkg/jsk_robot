f23ec015ceb499e4f0f7c1cdfa1f20159995ec51
Koki Shinjo <sktometometo@gmail.com>
2021-12-24 18:55:41 +0900

    [jsk_spot_startup] attach log to mail

--- jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
@@ -13,7 +13,7 @@ if [ -e $WORKSPACE_SPOT_DRIVER ]; then
     cd $WORKSPACE_SPOT_DRIVER/src
     wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_driver.rosinstall --merge-replace --confirm-all
     wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-    catkin build -v --pre-clean --no-status -j4 --continue-on-failure
+    catkin build --no-status -j4 --continue-on-failure > /tmp/build_spot_driver.log 2>&1
     if [ $? -eq 0 ]; then
         echo "Updating and building of $WORKSPACE_SPOT_DRIVER succeeded.\n" >> $MAIL_BODY_FILE
     else
@@ -21,13 +21,14 @@ if [ -e $WORKSPACE_SPOT_DRIVER ]; then
     fi
 else
     echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n" >> $MAIL_BODY_FILE
+    echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n" > /tmp/build_spot_driver.log
 fi
 
 if [ -e $WORKSPACE_SPOT_CORAL ]; then
     cd $WORKSPACE_SPOT_CORAL/src
     wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_coral.rosinstall --merge-replace --confirm-all
     wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-    catkin build -v --pre-clean --no-status -j4 --continue-on-failure
+    catkin build --no-status -j4 --continue-on-failure > /tmp/build_spot_coral.log 2>&1
     if [ $? -eq 0 ]; then
         echo "Updating and building of $WORKSPACE_SPOT_CORAL succeeded.\n" >> $MAIL_BODY_FILE
     else
@@ -35,13 +36,14 @@ if [ -e $WORKSPACE_SPOT_CORAL ]; then
     fi
 else
     echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n" >> $MAIL_BODY_FILE
+    echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n" > /tmp/build_spot_coral.log
 fi
 
 if [ -e $WORKSPACE_SPOT ]; then
     cd $WORKSPACE_SPOT/src
     wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_dev.rosinstall --merge-replace --confirm-all
     wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
-    catkin build -v --pre-clean --no-status -j4 --continue-on-failure
+    catkin build --no-status -j4 --continue-on-failure > /tmp/build_spot.log 2>&1
     if [ $? -eq 0 ]; then
         echo "Updating and building of $WORKSPACE_SPOT succeeded.\n" >> $MAIL_BODY_FILE
     else
@@ -49,8 +51,9 @@ if [ -e $WORKSPACE_SPOT ]; then
     fi
 else
     echo "Workspace $WORKSPACE_SPOT not found.\n" >> $MAIL_BODY_FILE
+    echo "Workspace $WORKSPACE_SPOT not found.\n" > /tmp/build_spot.log
 fi
 
 cat $MAIL_BODY_FILE
 
-cat $MAIL_BODY_FILE | mail -s "Workspace Updating Notification" -r spot-jsk@jsk.imi.i.u-tokyo.ac.jp spot@jsk.imi.i.u-tokyo.ac.jp
+cat $MAIL_BODY_FILE | mail -s "Workspace Updating Notification" -r spot-jsk@jsk.imi.i.u-tokyo.ac.jp spot@jsk.imi.i.u-tokyo.ac.jp -a /tmp/build_spot_driver.log -a /tmp/build_spot_coral.log -a /tmp/build_spot.log
