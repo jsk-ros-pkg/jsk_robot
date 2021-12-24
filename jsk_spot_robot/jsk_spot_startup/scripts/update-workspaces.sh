34cb97430efdd686ea7b3792da3f6e41680023f9
Koki Shinjo <sktometometo@gmail.com>
2021-12-24 18:57:35 +0900

    [jsk_spot_startup] fix update-workspace script

--- jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
@@ -16,11 +16,14 @@ if [ -e $WORKSPACE_SPOT_DRIVER ]; then
     catkin build --no-status -j4 --continue-on-failure > /tmp/build_spot_driver.log 2>&1
     if [ $? -eq 0 ]; then
         echo "Updating and building of $WORKSPACE_SPOT_DRIVER succeeded.\n" >> $MAIL_BODY_FILE
+        echo "Updating and building of $WORKSPACE_SPOT_DRIVER succeeded.\n"
     else
         echo "Updating and building of $WORKSPACE_SPOT_DRIVER failed.\n" >> $MAIL_BODY_FILE
+        echo "Updating and building of $WORKSPACE_SPOT_DRIVER failed.\n"
     fi
 else
     echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n" >> $MAIL_BODY_FILE
+    echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n"
     echo "Workspace $WORKSPACE_SPOT_DRIVER not found.\n" > /tmp/build_spot_driver.log
 fi
 
@@ -31,11 +34,14 @@ if [ -e $WORKSPACE_SPOT_CORAL ]; then
     catkin build --no-status -j4 --continue-on-failure > /tmp/build_spot_coral.log 2>&1
     if [ $? -eq 0 ]; then
         echo "Updating and building of $WORKSPACE_SPOT_CORAL succeeded.\n" >> $MAIL_BODY_FILE
+        echo "Updating and building of $WORKSPACE_SPOT_CORAL succeeded.\n"
     else
         echo "Updating and building of $WORKSPACE_SPOT_CORAL failed.\n" >> $MAIL_BODY_FILE
+        echo "Updating and building of $WORKSPACE_SPOT_CORAL failed.\n"
     fi
 else
     echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n" >> $MAIL_BODY_FILE
+    echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n"
     echo "Workspace $WORKSPACE_SPOT_CORAL not found.\n" > /tmp/build_spot_coral.log
 fi
 
@@ -44,13 +50,17 @@ if [ -e $WORKSPACE_SPOT ]; then
     wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_dev.rosinstall --merge-replace --confirm-all
     wstool update -t . -j 4 -m 60 --delete-changed-uris --continue-on-error
     catkin build --no-status -j4 --continue-on-failure > /tmp/build_spot.log 2>&1
+    cat /tmp/bulid_spot.log
     if [ $? -eq 0 ]; then
         echo "Updating and building of $WORKSPACE_SPOT succeeded.\n" >> $MAIL_BODY_FILE
+        echo "Updating and building of $WORKSPACE_SPOT succeeded.\n"
     else
         echo "Updating and building of $WORKSPACE_SPOT failed.\n" >> $MAIL_BODY_FILE
+        echo "Updating and building of $WORKSPACE_SPOT failed.\n"
     fi
 else
     echo "Workspace $WORKSPACE_SPOT not found.\n" >> $MAIL_BODY_FILE
+    echo "Workspace $WORKSPACE_SPOT not found.\n"
     echo "Workspace $WORKSPACE_SPOT not found.\n" > /tmp/build_spot.log
 fi
 
