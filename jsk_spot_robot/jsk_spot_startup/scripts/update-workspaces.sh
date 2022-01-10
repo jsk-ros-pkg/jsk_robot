79d1f9fd5b29178fb3a10166af6f6ec77ee5ee93
Koki Shinjo <sktometometo@gmail.com>
2022-01-10 11:12:57 +0900

    [jsk_spot_startup] fix workspace update script

--- jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-workspaces.sh
@@ -11,11 +11,12 @@ echo "Result of workspace updating  $(LANG=C; date) in $ROBOT_NAME\n" > $MAIL_BO
 
 function update_and_build () {
     local WORKSPACE=$1
-    local UPDATE_LOG=$2
-    local BUILD_LOG=$3
+    local ROSINSTALL_FILE=$2
+    local UPDATE_LOG=$3
+    local BUILD_LOG=$4
     if [ -e $WORKSPACE ]; then
         cd $WORKSPACE/src
-        wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/jsk_spot_driver.rosinstall --merge-replace --confirm-all
+        wstool merge -t . jsk-ros-pkg/jsk_robot/jsk_spot_robot/$ROSINSTALL_FILE --merge-replace --confirm-all
         wstool update -t . -j 4 -m 60 --delete-changed-uris 2>&1 | tee $UPDATE_LOG
         if [ $? -eq 0 ]; then
             echo "Updating of $WORKSPACE succeeded.\n" >> $MAIL_BODY_FILE
@@ -40,9 +41,9 @@ function update_and_build () {
     fi
 }
 
-update_and_build $WORKSPACE_SPOT_DRIVER /tmp/update_spot_driver.log /tmp/build_spot_driver.log
-update_and_build $WORKSPACE_SPOT_CORAL /tmp/update_spot_coral.log /tmp/build_spot_coral.log
-update_and_build $WORKSPACE_SPOT /tmp/update_spot.log /tmp/build_spot.log
+update_and_build $WORKSPACE_SPOT_DRIVER jsk_spot_driver.rosinstall /tmp/update_spot_driver.log /tmp/build_spot_driver.log
+update_and_build $WORKSPACE_SPOT_CORAL jsk_spot_coral.rosinstall /tmp/update_spot_coral.log /tmp/build_spot_coral.log
+update_and_build $WORKSPACE_SPOT jsk_spot_dev.rosinstall /tmp/update_spot.log /tmp/build_spot.log
 
 (uuencode /tmp/update_spot_driver.log /tmp/update_spot_driver.log;\
  uuencode /tmp/update_spot_coral.log /tmp/update_spot_coral.log;\
