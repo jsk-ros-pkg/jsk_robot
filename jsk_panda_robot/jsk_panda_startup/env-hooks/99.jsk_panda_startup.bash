6e648448e0a89161675f22b1849544f47251d8a9
Yoichiro Kawamura <y-kawamura@jsk.imi.i.u-tokyo.ac.jp>
2022-03-20 15:52:52 +0900

    add env-hooks

---
+++ jsk_panda_robot/jsk_panda_startup/env-hooks/99.jsk_panda_startup.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. "$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)/99.jsk_panda_startup.sh"
---
+++ jsk_panda_robot/jsk_panda_startup/env-hooks/99.jsk_panda_startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rossetpanda() {
+    rossetmaster dual_panda.jsk.imi.i.u-tokyo.ac.jp
+    rossetip
+}
