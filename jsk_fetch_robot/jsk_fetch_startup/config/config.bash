1c754bab868769ba7a2477bd9ac6eee05da26e14
Koki Shinjo <sktometometo@gmail.com>
2022-04-28 12:44:10 +0900

    [jsk_fetch_startup] enable to change robot_description according to config.bash

--- jsk_fetch_robot/jsk_fetch_startup/config/config.bash
+++ jsk_fetch_robot/jsk_fetch_startup/config/config.bash
@@ -1,6 +1,11 @@
 # This file is bash configuration for robot.conf
 # This file must be at /var/lib/robot/config.bash
 
+export USE_BASE_CAMERA_MOUNT=true
+export USE_HEAD_BOX=true
+export USE_HEAD_L515=true
+export USE_INSTA360_STAND=true
+
 export RS_SERIAL_NO_T265=""
 export RS_SERIAL_NO_D435_FRONTRIGHT=""
 export RS_SERIAL_NO_D435_FRONTLEFT=""
