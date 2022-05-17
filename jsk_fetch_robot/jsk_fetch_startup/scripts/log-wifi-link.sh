c751e6322a08523dd4ee549adcc717235a750bab
Koki Shinjo <sktometometo@gmail.com>
2022-05-17 23:18:07 +0900

    [jsk_fetch_startup] add interface arg to log-wifi-link.sh

--- jsk_fetch_robot/jsk_fetch_startup/scripts/log-wifi-link.sh
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/log-wifi-link.sh
@@ -7,9 +7,9 @@ INTERVAL=10
 wifi_status(){
     local ssid=$(iw $IFACE link | grep SSID | cut -d: -f2)
     local freq=$(iw $IFACE link | grep freq | cut -d':' -f2)
-    local signal=$(cat /proc/net/wireless | grep wlan0 | awk '{print $3}')
-    local level=$(cat /proc/net/wireless | grep wlan0 | awk '{print $4}')
-    local noise=$(cat /proc/net/wireless | grep wlan0 | awk '{print $5}')
+    local signal=$(cat /proc/net/wireless | grep $IFACE | awk '{print $3}')
+    local level=$(cat /proc/net/wireless | grep $IFACE | awk '{print $4}')
+    local noise=$(cat /proc/net/wireless | grep $IFACE | awk '{print $5}')
     local bitrate=$(iw $IFACE link | grep bitrate | cut -d':' -f2)
     echo "$(date -Iseconds),$ssid,$freq,$signal,$level,$noise,$bitrate"
 }
@@ -18,6 +18,20 @@ OUTDIR=$(dirname $OUTPATH)
 if [ ! -d $OUTDIR ]; then
     mkdir -p $OUTDIR
 fi
+
+print_help(){
+    echo "Usage: $0 [-i INTERVAL (default: 10)] [-I IFACE (default: wlan0)]"
+    exit 1
+}
+
+while getopts hi:I: o
+do
+    case "$o" in
+        i)  INTERVAL=$OPTARG;;
+        I)  IFACE="$OPTARG";;
+        h)  print_help;;
+    esac
+done
 
 while true; do
     wifi_status >> $OUTPATH
