477537958b8a087027f0606d9639712cbe0940fe
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-05-25 17:26:52 +0900

    disable c2 for debugging

--- jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/robot.bash
+++ jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/robot.bash
@@ -8,5 +8,5 @@ echo "ROS_ENV_LOADER: $ROS_ENV_LOADER";
 robot claim -f;
 robot stop -f;
 sleep 3;
-$ROS_ENV_LOADER roslaunch /etc/ros/robot.launch --pid /var/tmp/ros.id --run_id=$ROS_RUN_ID
+$ROS_ENV_LOADER roslaunch /etc/ros/robot.launch c2:=false --pid /var/tmp/ros.id --run_id=$ROS_RUN_ID
 set +x
