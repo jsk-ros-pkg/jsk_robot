2c0e0ce86951d2210c9e64ba48aff9de912e71c2
JSK fetch user <fetch@jsk.imi.i.u-tokyo.ac.jp>
2022-05-26 16:15:37 +0900

    add config_outside.bash

---
+++ jsk_fetch_robot/jsk_fetch_startup/config/config_outside.bash
@@ -0,0 +1,13 @@
+# This file is bash configuration for robot.conf
+# This file must be at /var/lib/robot/config.bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+. $SCRIPT_DIR/config.bash
+
+if [ $(hostname) = 'fetch15' ]; then
+  export NETWORK_DEFAULT_PROFILE_ID="sanshiro-outside";
+elif [ $(hostname) = 'fetch1075' ]; then
+  export NETWORK_DEFAULT_WIFI_INTERFACE="wlan1";
+  export NETWORK_DEFAULT_ROS_INTERFACE="wlan0";
+  export NETWORK_DEFAULT_PROFILE_ID="sanshiro-outside";
+fi
