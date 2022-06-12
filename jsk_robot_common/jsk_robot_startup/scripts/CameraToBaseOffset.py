befb191381cbbb361cd6ea2a4f84cc8bf277275a
Koki Shinjo <sktometometo@gmail.com>
2022-06-12 14:49:42 +0900

    [jsk_robot_startup] refactor variable names of CameraBaseToOffset.py

--- jsk_robot_common/jsk_robot_startup/scripts/CameraToBaseOffset.py
+++ jsk_robot_common/jsk_robot_startup/scripts/CameraToBaseOffset.py
@@ -21,19 +21,24 @@ from jsk_robot_startup.odometry_utils import make_homogeneous_matrix
 class CameraToBaseOffset(object):
     def __init__(self):
         rospy.init_node("CameraToBaseOffset", anonymous=True)
+
+        # frame_id
+        self.base_frame_id = rospy.get_param("~base_frame_id", "BODY")
+        self.camera_frame_id = rospy.get_param("~camera_frame_id", "left_camera_optical_frame")
+        self.odom_frame_id = rospy.get_param("~odom_frame_id", "viso_odom")
+
         # tf parameters
         self.publish_tf = rospy.get_param("~publish_tf", True)
-        if self.publish_tf:
-            self.invert_tf = rospy.get_param("~invert_tf", True)
-            self.broadcast = tf.TransformBroadcaster()
-        self.base_link_frame = rospy.get_param("~base_link_frame", "BODY")
-        self.camera_frame = rospy.get_param(
-            "~camera_frame", "left_camera_optical_frame")
-        self.odom_frame = rospy.get_param("~odom_frame", "viso_odom")
+        self.invert_tf = rospy.get_param("~invert_tf", True)
         self.tf_duration = rospy.get_param("~tf_duration", 1)
+
+        # other members
+        if self.publish_tf:
+            self.broadcast = tf.TransformBroadcaster()
         self.listener = tf.TransformListener(True, rospy.Duration(10))
-        self.initial_base_to_odom_transformation = None
+        self.htm_odom_base_to_odom_camera = None
         self.lock = threading.Lock()
+
         self.source_odom_sub = rospy.Subscriber(
             "~source_odom", Odometry, self.source_odom_callback)
         self.init_signal_sub = rospy.Subscriber(
@@ -46,74 +51,89 @@ class CameraToBaseOffset(object):
     def init_signal_callback(self, msg):
         time.sleep(1)  # wait to update odom_init frame
         with self.lock:
-            self.initial_base_to_odom_transformation = None
+            self.htm_odom_base_to_odom_camera = None
 
     def source_odom_callback(self, msg):
         with self.lock:
             # calculate camera transform
-            current_camera_to_base = self.calculate_camera_to_base_transform(
-                msg.header.stamp)
-            if current_camera_to_base is None:
+            htm_camera_to_base = self.get_htm_camera_to_base(msg.header.stamp)
+            if htm_camera_to_base is None:
                 return
 
-            source_odom_matrix = make_homogeneous_matrix(
+            # htm (Homogeneous Transformation Matrix)
+            #   from odom frame of camera to current camera frame
+            htm_odom_camera_to_camera = make_homogeneous_matrix(
                     [msg.pose.pose.position.x,
                      msg.pose.pose.position.y,
                      msg.pose.pose.position.z],
                     [msg.pose.pose.orientation.x,
                      msg.pose.pose.orientation.y,
                      msg.pose.pose.orientation.z,
-                     msg.pose.pose.orientation.w])
+                     msg.pose.pose.orientation.w]
+                    )
 
-            if self.initial_base_to_odom_transformation is None:
-                self.initial_base_to_odom_transformation = numpy.linalg.inv(
-                    numpy.dot(source_odom_matrix, current_camera_to_base))
+            if self.htm_odom_base_to_odom_camera is None:
+                self.htm_odom_base_to_odom_camera = numpy.linalg.inv(
+                    numpy.dot(htm_odom_camera_to_camera, htm_camera_to_base))
 
             # calculate offseted odometry
-            current_odom_to_base = numpy.dot(
-                source_odom_matrix, current_camera_to_base)
-            odom_relative_base_transformation = numpy.dot(
-                self.initial_base_to_odom_transformation, current_odom_to_base)
-            new_odom_matrix = odom_relative_base_transformation
+            htm_odom_camera_to_base = numpy.dot(
+                htm_odom_camera_to_camera, htm_camera_to_base)
+            htm_odom_base_to_base = numpy.dot(
+                self.htm_odom_base_to_odom_camera, htm_odom_camera_to_base)
+
+            # calculate transformed pose covariance
+            transformed_pose_cov_matrix = numpy.matrix(msg.pose.covariance).reshape(6, 6)
+            rotation_matrix = htm_odom_base_to_base[:3, :3]
+            transformed_pose_cov_matrix[:3, :3] = (rotation_matrix.T).dot(
+                transformed_pose_cov_matrix[:3, :3].dot(rotation_matrix))
+            transformed_pose_cov_matrix[3:6, 3:6] = (rotation_matrix.T).dot(
+                transformed_pose_cov_matrix[3:6, 3:6].dot(rotation_matrix))
 
             # make odometry msg. twist is copied from source_odom
-            new_odom = copy.deepcopy(msg)
-            new_odom.header.frame_id = self.odom_frame
-            new_odom.child_frame_id = self.base_link_frame
-            new_odom.pose.pose.position = Point(*list(new_odom_matrix[:3, 3]))
-            new_odom.pose.pose.orientation = Quaternion(
-                *list(tf.transformations.quaternion_from_matrix(new_odom_matrix)))
-            new_pose_cov_matrix = numpy.matrix(
-                new_odom.pose.covariance).reshape(6, 6)
-            rotation_matrix = odom_relative_base_transformation[:3, :3]
-            new_pose_cov_matrix[:3, :3] = (rotation_matrix.T).dot(
-                new_pose_cov_matrix[:3, :3].dot(rotation_matrix))
-            new_pose_cov_matrix[3:6, 3:6] = (rotation_matrix.T).dot(
-                new_pose_cov_matrix[3:6, 3:6].dot(rotation_matrix))
-            new_odom.pose.covariance = numpy.array(
-                new_pose_cov_matrix).reshape(-1,).tolist()
+            transformed_odom_msg = copy.deepcopy(msg)
+            transformed_odom_msg.header.frame_id = self.odom_frame_id
+            transformed_odom_msg.child_frame_id = self.base_frame_id
+            transformed_odom_msg.pose.pose.position = Point(*list(htm_odom_base_to_base[:3, 3]))
+            transformed_odom_msg.pose.pose.orientation = Quaternion(
+                *list(tf.transformations.quaternion_from_matrix(htm_odom_base_to_base)))
+            transformed_odom_msg.pose.covariance = numpy.array(transformed_pose_cov_matrix).reshape(-1,).tolist()
 
             # publish
-            self.pub.publish(new_odom)
+            self.pub.publish(transformed_odom_msg)
             if self.publish_tf:
-                broadcast_transform(self.broadcast, new_odom, self.invert_tf)
+                broadcast_transform(self.broadcast, transformed_odom_msg, self.invert_tf)
 
-    def calculate_camera_to_base_transform(self, stamp):
+    def get_htm_camera_to_base(self, stamp):
         try:
             (trans, rot) = self.listener.lookupTransform(
-                self.camera_frame, self.base_link_frame, stamp)
-        except:
+                    self.camera_frame_id,
+                    self.base_frame_id,
+                    stamp)
+        except (tf.LookupException,
+                tf.ConnectivityException,
+                tf.ExtrapolationException):
             try:
-                # rospy.logwarn("[%s] failed to solve camera_to_base tf in %f. Use rospy.Time(0): %s to %s", rospy.get_name(), stamp.to_sec(), self.camera_frame, self.base_link_frame)
+                rospy.logwarn(
+                        "[%s] failed to solve camera_to_base tf in %f. Use rospy.Time(0): %s to %s",
+                        rospy.get_name(),
+                        stamp.to_sec(),
+                        self.camera_frame_id,
+                        self.base_frame_id)
                 (trans, rot) = self.listener.lookupTransform(
-                    self.camera_frame, self.base_link_frame, rospy.Time(0))
-            except:
-                rospy.logwarn("[%s] failed to solve camera_to_base tf: %s to %s", rospy.get_name(
-                ), self.camera_frame, self.base_link_frame)
+                    self.camera_frame_id,
+                    self.base_frame_id,
+                    rospy.Time(0))
+            except (tf.LookupException,
+                    tf.ConnectivityException,
+                    tf.ExtrapolationException):
+                rospy.logwarn(
+                        "[%s] failed to solve camera_to_base tf: %s to %s",
+                        rospy.get_name(),
+                        self.camera_frame_id,
+                        self.base_frame_id)
                 return None
-        camera_to_base_link = make_homogeneous_matrix(
-            trans, rot)  # camera -> base_link
-        return camera_to_base_link
+        return make_homogeneous_matrix(trans, rot)
 
 
 if __name__ == '__main__':
