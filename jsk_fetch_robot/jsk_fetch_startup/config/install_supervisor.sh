4b6be555397bd6ea35b619cbfc3cff711df3d82d
Koki Shinjo <sktometometo@gmail.com>
2022-06-27 14:17:20 +0900

    [jsk_fetch_startup] add -f to ln in install_supervisor.sh

--- jsk_fetch_robot/jsk_fetch_startup/config/install_supervisor.sh
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_supervisor.sh
@@ -9,13 +9,13 @@ set -x
 
 cd $jsk_fetch_startup/supervisor_scripts
 for file in $(ls *.conf); do
-    sudo ln -s $jsk_fetch_startup/supervisor_scripts/$file /etc/supervisor/conf.d/$file
+    sudo ln -sf $jsk_fetch_startup/supervisor_scripts/$file /etc/supervisor/conf.d/$file
 done
 sudo supervisorctl reread
 # Enable jsk_dstat job to save the csv log under /var/log
-ln -s /home/fetch/Documents/jsk_dstat.csv /var/log/ros/jsk-dstat.csv
+ln -sf /home/fetch/Documents/jsk_dstat.csv /var/log/ros/jsk-dstat.csv
 
-sudo ln -s $jsk_fetch_startup/config/config.bash /var/lib/robot/config.bash
-sudo ln -s $jsk_fetch_startup/config/config.bash /var/lib/robot/config_outside.bash
+sudo ln -sf $jsk_fetch_startup/config/config.bash /var/lib/robot/config.bash
+sudo ln -sf $jsk_fetch_startup/config/config.bash /var/lib/robot/config_outside.bash
 
 set +x
