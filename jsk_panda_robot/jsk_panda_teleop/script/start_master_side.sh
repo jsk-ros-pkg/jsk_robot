11cca5af14ec7b9c5c8f8121882fa885a7873662
Shun Hasegawa <pazeshun3684@gmail.com>
2022-07-19 14:59:40 +0900

    [jsk_panda_teleop] Delete unnecessary command about phantom omni device connection
    The interface of our device is USB, so we do not need to run a command for FireWire.
    See http://fsuarez6.github.io/projects/geomagic-touch-in-ros/#allow-access-to-the-firewire-interface

--- jsk_panda_robot/jsk_panda_teleop/script/start_master_side.sh
+++ jsk_panda_robot/jsk_panda_teleop/script/start_master_side.sh
@@ -5,8 +5,6 @@ function init_device() {
     echo "giving access to /dev/tty0 and /dev/tty1"
     # TODO: add udev rule to remove sudo's
     sudo chmod 777 /dev/ttyACM0 && sudo chmod 777 /dev/ttyACM1
-    # Run below if you use Omini for the first ime
-    # rosrun omni_common initialize_device.sh 
 }
 
 function start_master_launch () {
