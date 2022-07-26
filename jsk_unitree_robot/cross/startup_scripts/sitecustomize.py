554a87bb09f05793adc3a0d29fc82159d55cc908
iory <ab.ioryz@gmail.com>
2022-07-26 16:05:12 +0900

    [jsk_unitree_startup/cross] Recursively append python site/dist-package paths

--- jsk_unitree_robot/cross/startup_scripts/sitecustomize.py
+++ jsk_unitree_robot/cross/startup_scripts/sitecustomize.py
@@ -1,6 +1,7 @@
 # install the apport exception handler if available
 import site
 import sys
+import os
 import os.path as osp
 
 site.PREFIXES += [
@@ -9,13 +10,25 @@ site.PREFIXES += [
     '/opt/jsk/System/ros1_inst/',
 ]
 
+
+def recursively_listup_paths(path):
+    paths = []
+    for sub_path in os.listdir(path):
+        sub_full_path = osp.join(path, sub_path)
+        if osp.isdir(sub_full_path) and (sub_path.endswith('.egg-info') or sub_path.endswith('.egg')):
+            paths.append(sub_full_path)
+    return paths
+
+
 paths = []
 for path in site.getsitepackages():
+    if osp.exists(path):
+        paths.append(path)
+        paths.extend(recursively_listup_paths(path))
     site_package = osp.join(osp.dirname(path), 'site-packages')
     if site_package != path and osp.exists(site_package):
         paths.append(site_package)
-    else:
-        paths.append(path)
+        paths.extend(recursively_listup_paths(site_package))
 
 sys.path.extend(paths)
 try:
