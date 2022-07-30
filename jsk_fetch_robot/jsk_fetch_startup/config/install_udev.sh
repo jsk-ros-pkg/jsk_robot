d4fbe91b629015cfeb2092f5e71d52a6aa5571fa
Naoya Yamaguchi <708yamaguchi@gmail.com>
2022-07-30 17:29:01 +0900

    [jsk_fetch_startup] Use symbolic link for udev rules file, as well as supervisor configs

--- jsk_fetch_robot/jsk_fetch_startup/config/install_udev.sh
+++ jsk_fetch_robot/jsk_fetch_startup/config/install_udev.sh
@@ -4,14 +4,6 @@ jsk_fetch_startup=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`"/.. > /dev/null &&
 
 cd $jsk_fetch_startup/udev_rules
 for file in $(ls *.rules); do
-    if [ -e /etc/udev/rules.d/$file ]; then
-        file_bk=$file.$(date "+%Y%m%d_%H%M%S")
-        sudo cp /etc/udev/rules.d/$file /etc/udev/rules.d/$file_bk
-        echo "backup /etc/udev/rules.d/$file to /etc/udev/rules.d/$file_bk"
-    fi
-
-    sudo cp $file /etc/udev/rules.d/
-    sudo chown root:root /etc/udev/rules.d/$file
-    sudo chmod 644 /etc/udev/rules.d/$file
-    echo -e "copied jsk_fetch_startup/udev_rules/$file to /etc/udev/rules.d/$file\n"
+    sudo ln -sf $jsk_fetch_startup/udev_rules/$file /etc/udev/rules.d/$file
+    echo -e "Create symbolic link from jsk_fetch_startup/udev_rules/$file to /etc/udev/rules.d/$file\n"
 done
