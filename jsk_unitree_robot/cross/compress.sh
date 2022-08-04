722b3fbcda1d9e99f8b9b9cff3581cec2a9f5e0a
iory <ab.ioryz@gmail.com>
2022-08-05 00:11:29 +0900

    [jsk_unitree_startup/cross] Add y or n

--- jsk_unitree_robot/cross/compress.sh
+++ jsk_unitree_robot/cross/compress.sh
@@ -2,12 +2,27 @@
 
 if [ -e "arm64v8_User" ]; then
     if [ -e "arm64v8_User.tar.gz" ]; then
+        echo "WARNING: Compressed arm64v8_User.tar.gz is found."
+        read -p "WARNING: Are you sure to continue [y/N] ? " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            tar -zcvf arm64v8_User.tar.gz arm64v8_User
+        fi
+    else
         tar -zcvf arm64v8_User.tar.gz arm64v8_User
     fi
 fi
 
 if [ -e "arm64v8_System" ]; then
     if [ -e "arm64v8_System.tar.gz" ]; then
+        echo "WARNING: Compressed arm64v8_System.tar.gz is found."
+        read -p "WARNING: Are you sure to continue [y/N] ? " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            chmod 644 arm64v8_System/ros1_inst/share/pr2eus/*.l
+            tar -zcvf arm64v8_System.tar.gz arm64v8_System
+        fi
+    else
         chmod 644 arm64v8_System/ros1_inst/share/pr2eus/*.l
         tar -zcvf arm64v8_System.tar.gz arm64v8_System
     fi
