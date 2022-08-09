997b0975369df63030e0721120c0872514443ec9
Kei Okada <kei.okada@gmail.com>
2022-08-09 11:51:33 +0900

    enable to receive multiple smach state

--- jsk_robot_common/jsk_robot_startup/scripts/smach_to_mail.py
+++ jsk_robot_common/jsk_robot_startup/scripts/smach_to_mail.py
@@ -25,7 +25,8 @@ class SmachToMail():
         rospy.Subscriber(
             "~smach/container_status", SmachContainerStatus, self._status_cb)
         self.bridge = CvBridge()
-        self.smach_state_list = None  # for status list
+        self.smach_state_list = {}  # for status list
+        self.smach_state_subject = {}  # for status subject
         self.sender_address = "tsukamoto@jsk.imi.i.u-tokyo.ac.jp"
         self.receiver_address = "tsukamoto@jsk.imi.i.u-tokyo.ac.jp"
 
@@ -36,7 +37,8 @@ class SmachToMail():
         rospy.loginfo("Received SMACH status")
         if len(msg.active_states) == 0:
             return
-        file_path = None
+
+        # Print received messages
         status_str = ', '.join(msg.active_states);
         local_data_str = pickle.loads(msg.local_data)
         info_str = msg.info
@@ -54,15 +56,21 @@ class SmachToMail():
         if local_data_str.has_key('IMAGE') and local_data_str['IMAGE']:
             rospy.loginfo("- image_str -> {}".format(local_data_str['IMAGE'][:64]))
 
+        # Store data for every callerid to self.smach_state_list[caller_id]
+        caller_id = msg._connection_header['callerid']
+
+        # If we received START/INIT status, restart storing smach_state_list
         if status_str in ["START", "INIT"]:
-            self.smach_state_list = []
+            self.smach_state_list[caller_id] = []
             # DESCRIPTION of START is MAIL SUBJECT
             if local_data_str.has_key('DESCRIPTION'):
-                self.smach_state_subject = local_data_str['DESCRIPTION']
+                self.smach_state_subject[caller_id] = local_data_str['DESCRIPTION']
                 del local_data_str['DESCRIPTION']
             else:
-                self.smach_state_subject = None
+                self.smach_state_subject[celler_id] = None
 
+        # Build status_dict for every status
+        # expected keys are 'DESCRIPTION' , 'IMAGE', 'STATE', 'INFO', 'TIME'
         status_dict = {}
         if local_data_str.has_key('DESCRIPTION'):
             status_dict.update({'DESCRIPTION': local_data_str['DESCRIPTION']})
@@ -77,28 +85,36 @@ class SmachToMail():
             dim = (width, height)
             cv_image = cv2.resize(cv_image, dim, interpolation = cv2.INTER_AREA)
             status_dict.update({'IMAGE': base64.b64encode(cv2.imencode('.jpg', cv_image)[1].tostring())})  # dict is complicated?
+        status_dict.update({'STATE': status_str})
+        status_dict.update({'INFO': info_str})
+        status_dict.update({'TIME': datetime.datetime.fromtimestamp(msg.header.stamp.to_sec())})
 
-        if self.smach_state_list == None:
+        if ( not self.smach_state_list.has_key(caller_id) ) or self.smach_state_list[caller_id] == None:
             rospy.logwarn("received {}, but we did not find START node".format(status_dict))
         else:
-            self.smach_state_list.append(status_dict)
+            self.smach_state_list[caller_id].append(status_dict)
 
+        # If we received END/FINISH status, send email, etc...
         if status_str in ["END", "FINISH"]:
-            if self.smach_state_list == None:
+            if ( not self.smach_state_list.has_key(caller_id) ) or self.smach_state_list[caller_id] == None:
                 rospy.logwarn("received END node, but we did not find START node")
                 rospy.logwarn("failed to send {}".format(status_dict))
             else:
                 rospy.loginfo("END!!")
-                self._send_mail()
-                self.smach_state_list = None
+                rospy.loginfo("Following SMACH is reported")
+                for x in self.smach_state_list[caller_id]:
+                    rospy.loginfo(" - At {}, Active state is {}{}".format(x['TIME'], x['STATE'],
+                                  "({})".format(x['INFO']) if x['INFO'] else ''))
+                self._send_mail(self.smach_state_subject[caller_id], self.smach_state_list[caller_id])
+                self.smach_state_list[caller_id] = None
 
-    def _send_mail(self):
+    def _send_mail(self, subject, state_list):
         email_msg = Email()
         email_msg.body = []
         changeline = EmailBody()
         changeline.type = 'html'
         changeline.message = "<br>"
-        for x in self.smach_state_list:
+        for x in state_list:
             if x.has_key('DESCRIPTION'):
                 description = EmailBody()
                 description.type = 'text'
@@ -114,8 +130,8 @@ class SmachToMail():
                 email_msg.body.append(changeline)
         # rospy.loginfo("body:{}".format(email_msg.body))
 
-        if self.smach_state_subject:
-            email_msg.subject = self.smach_state_subject
+        if subject:
+            email_msg.subject = subject
         else:
             email_msg.subject = 'Message from {}'.format(rospy.get_param('/robot/name', 'robot'))
 
