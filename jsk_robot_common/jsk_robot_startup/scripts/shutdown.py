dddc2f89d77e7fe6a86f548127e19d9088d52cac
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-08-18 03:00:29 +0900

    [jsk_robot_startup] Speak when shut down or reboot (#319)

--- jsk_robot_common/jsk_robot_startup/scripts/shutdown.py
+++ jsk_robot_common/jsk_robot_startup/scripts/shutdown.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import rospy
+from sound_play.libsoundplay import SoundClient
 from std_msgs.msg import Empty
-
 
 class Shutdown(object):
     """
@@ -24,15 +25,22 @@ class Shutdown(object):
 
     def __init__(self):
         rospy.loginfo('Start shutdown node.')
+        self.client_jp = SoundClient(sound_action='/robotsound_jp', blocking=True)
         rospy.Subscriber('shutdown', Empty, self.shutdown)
         rospy.Subscriber('reboot', Empty, self.reboot)
         self.shutdown_command = rospy.get_param(
             '~shutdown_command', '/sbin/shutdown -h now')
         self.reboot_command = rospy.get_param(
             '~reboot_command', '/sbin/shutdown -r now')
+        self.volume = rospy.get_param('~volume', 1.0)
+
+    def speak(self, client, speech_text, lang='jp'):
+        client.say(speech_text, voice=lang, volume=self.volume, replace=False)
+        return client.actionclient.get_result()
 
     def shutdown(self, msg):
         rospy.loginfo('Shut down robot.')
+        self.speak(self.client_jp, 'シャットダウンします。')
         ret = os.system(self.shutdown_command)
         if ret != 0:
             rospy.logerr("Failed to call '$ {}'. Check authentication.".format(
@@ -40,6 +48,7 @@ class Shutdown(object):
 
     def reboot(self, msg):
         rospy.loginfo('Reboot robot.')
+        self.speak(self.client_jp, '再起動します。')
         ret = os.system(self.reboot_command)
         if ret != 0:
             rospy.logerr("Failed to call '$ {}'. Check authentication.".format(
