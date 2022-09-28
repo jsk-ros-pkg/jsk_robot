f1f185720dfeda76b3328ac501c15bd312757e5f
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-09-29 00:22:54 +0900

    [jsk_robot_startup] Automatically get sysdefault device

--- jsk_robot_common/jsk_robot_startup/systemd/playsound.sh
+++ jsk_robot_common/jsk_robot_startup/systemd/playsound.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo $0
+read device < <(aplay -L|grep sysdefault)
+echo $device
 if (echo $0|grep 'stop_sound.sh' > /dev/null); then
-    echo -n "Stopping sound: "
+    echo -n "Shutdown sound: "
     if type aplay &> /dev/null && [ -f "/usr/share/sounds/jsk_robot_startup/stop_sound.wav" ]; then
-        aplay --device=sysdefault:CARD=AUDIO /usr/share/sounds/jsk_robot_startup/stop_sound.wav
+        aplay --device=$device /usr/share/sounds/jsk_robot_startup/stop_sound.wav
     elif type beep &> /dev/null; then
         beep -f 783.99 -l 250 -n -f 659.26 -l 250 -n -f 587.33 -l 250 -n -f 523.25 -l 500
     fi
 elif (echo $0|grep 'start_sound.sh' > /dev/null); then
-    echo -n "Starting sound: "
+    echo -n "Boot sound: "
     if type aplay &> /dev/null && [ -f "/usr/share/sounds/jsk_robot_startup/start_sound.wav" ]; then
-        aplay --device=sysdefault:CARD=AUDIO /usr/share/sounds/jsk_robot_startup/start_sound.wav
+        aplay --device=$device /usr/share/sounds/jsk_robot_startup/start_sound.wav
     elif type beep &> /dev/null; then
         beep -f 523.25 -l 250 -n -f 587.33 -l 250 -n -f 659.26 -l 250 -n -f 783.99 -l 500
     fi
