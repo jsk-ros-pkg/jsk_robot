ea45f51381d911d8eab02887e253952df177c198
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-09-30 17:56:50 +0900

    [jsk_pr2_startup] do not run rosdep in pr2

--- jsk_pr2_robot/jsk_pr2_startup/scripts/update_workspace.sh
+++ jsk_pr2_robot/jsk_pr2_startup/scripts/update_workspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cp $(rospack find jsk_robot_startup)/scripts/update_workspace_main.sh /tmp/update_workspace.sh
-/tmp/update_workspace.sh -w $HOME/ros/indigo -r $(rospack find jsk_pr2_startup)/jsk_pr2.rosinstall -t pr2
+/tmp/update_workspace.sh -w $HOME/ros/indigo -r $(rospack find jsk_pr2_startup)/jsk_pr2.rosinstall -t pr2 -u
 rm /tmp/update_workspace.sh
