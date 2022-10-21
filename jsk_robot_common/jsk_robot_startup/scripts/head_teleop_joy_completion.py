966c49ee498014e80b442a5c52212794a5767022
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2022-10-21 11:32:38 +0900

    support min/max of x,y,yaw

--- jsk_robot_common/jsk_robot_startup/scripts/head_teleop_joy_completion.py
+++ jsk_robot_common/jsk_robot_startup/scripts/head_teleop_joy_completion.py
@@ -58,9 +58,9 @@ class JoyTopicCompletion:
             rospy.loginfo("raw: x={:5.2f}, y={:5.2f}, v={:5.2f}, r={:5.2f} -> cmd: x={:5.2f}, y={:5.2f}, r={:5.2f}".format(x, y, v, r, xx, yy, rr))
 
             msg.axes = list(msg.axes)  # tuple to list
-            msg.axes[self.axis_linear_x] = xx
-            msg.axes[self.axis_linear_y] = yy
-            msg.axes[self.axis_angular] = rr
+            msg.axes[self.axis_linear_x] = max(min(xx, self.axis_linear_x_max), self.axis_linear_x_min)
+            msg.axes[self.axis_linear_y] = max(min(yy, self.axis_linear_y_max), self.axis_linear_y_min)
+            msg.axes[self.axis_angular] = max(min(rr, self.axis_angular_max), self.axis_angular_min)
 
         self.pub.publish(msg)
 
@@ -74,6 +74,13 @@ class JoyTopicCompletion:
             if rospy.Time.now() - self.last_publish_time > rospy.Duration(3.0):
                 self.pub.publish(msg)
 
+    def get_param(self, key1, key2, value):
+        p = rospy.get_param(key1)
+        if type(p) == dict and p.has_key(key2):
+            return p[key2]
+        else:
+            return value
+
     def __init__(self):
         rospy.init_node('joy_topic_completion')
         self.pass_through = rospy.get_param('~pass_through', True)
@@ -81,6 +88,12 @@ class JoyTopicCompletion:
         self.axis_linear_x = int(rospy.get_param('~axis_linear', {'x': 1})['x'])
         self.axis_linear_y = int(rospy.get_param('~axis_linear', {'y': 2})['y'])
         self.axis_angular = int(rospy.get_param('~axis_angular', {'yaw': 0})['yaw'])
+        self.axis_linear_x_min = float(self.get_param('~axis_linear', 'x_min',  -1))
+        self.axis_linear_x_max = float(self.get_param('~axis_linear', 'x_max',   1))
+        self.axis_linear_y_min = float(self.get_param('~axis_linear', 'y_min',  -1))
+        self.axis_linear_y_max = float(self.get_param('~axis_linear', 'y_max',   1))
+        self.axis_angular_min = float(self.get_param('~axis_angular', 'yaw_min', -0.5))
+        self.axis_angular_max = float(self.get_param('~axis_angular', 'yaw_max',  0.5))
 
         self.last_publish_time = rospy.Time.now()
 
