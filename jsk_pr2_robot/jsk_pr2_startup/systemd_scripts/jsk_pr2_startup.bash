8ae75fe12695946b4dd5baa8899480498af35328
Shingo Kitagawa <shingogo@hotmail.co.jp>
2023-02-04 22:21:57 +0900

    update systemd jsk_pr2_startup scripts

--- jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/jsk_pr2_startup.bash
+++ jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/jsk_pr2_startup.bash
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-exec su applications -c ". /tmp/ros_run_id; ${ROS_ENV_LOADER} roslaunch jsk_pr2_startup pr2.launch --screen --wait map_frame:=${MAP_FRAME}"
+set -x
+. /tmp/ros_run_id
+$ROS_ENV_LOADER roslaunch jsk_pr2_startup pr2.launch --screen --wait map_frame:=$MAP_FRAME
+set +x
