f8e2d4ea08395d8650923e0698e4c94941c6d1d7
Shingo Kitagawa <shingogo@hotmail.co.jp>
2023-02-08 17:20:29 +0900

    add more echo in robot.bash

--- jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/robot.bash
+++ jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/robot.bash
@@ -3,6 +3,8 @@
 set -x
 . /tmp/ros_run_id
 . /var/lib/robot/config.bash
+echo "ROS_IP: $ROS_IP";
+echo "ROS_MASTER_URI: $ROS_MASTER_URI";
 echo "ROS_RUN_ID: $ROS_RUN_ID";
 echo "ROS_ENV_LOADER: $ROS_ENV_LOADER";
 robot claim -f;
