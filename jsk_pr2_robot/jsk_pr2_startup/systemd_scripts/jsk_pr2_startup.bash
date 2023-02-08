de259d77dfffb2f6fdf2f4aa099b2130718f9cd1
Shingo Kitagawa <shingogo@hotmail.co.jp>
2023-02-08 17:21:14 +0900

    add more echo in jsk_pr2_startup.bash

--- jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/jsk_pr2_startup.bash
+++ jsk_pr2_robot/jsk_pr2_startup/systemd_scripts/jsk_pr2_startup.bash
@@ -3,5 +3,9 @@
 set -x
 . /tmp/ros_run_id
 . /var/lib/robot/config.bash
+echo "ROS_IP: $ROS_IP";
+echo "ROS_MASTER_URI: $ROS_MASTER_URI";
+echo "ROS_RUN_ID: $ROS_RUN_ID";
+echo "ROS_ENV_LOADER: $ROS_ENV_LOADER";
 $ROS_ENV_LOADER roslaunch jsk_pr2_startup pr2.launch --screen --wait map_frame:=$MAP_FRAME
 set +x
