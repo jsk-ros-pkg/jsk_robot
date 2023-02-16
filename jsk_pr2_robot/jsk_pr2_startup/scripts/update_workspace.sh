89c0a6130d029720bd0f852ac63fa6a8a5711f10
Shingo Kitagawa <shingogo@hotmail.co.jp>
2023-02-16 16:44:29 +0900

    update_workspace script for noetic

--- jsk_pr2_robot/jsk_pr2_startup/scripts/update_workspace.sh
+++ jsk_pr2_robot/jsk_pr2_startup/scripts/update_workspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cp $(rospack find jsk_robot_startup)/scripts/update_workspace_main.sh /tmp/update_workspace.sh
-/tmp/update_workspace.sh -w $HOME/ros/indigo -r $(rospack find jsk_pr2_startup)/jsk_pr2.rosinstall -t pr2 -u
+/tmp/update_workspace.sh -w $HOME/ros/$ROS_DISTRO -r $(rospack find jsk_pr2_startup)/jsk_pr2.rosinstall.$ROS_DISTRO -t pr2 -u
 rm /tmp/update_workspace.sh
