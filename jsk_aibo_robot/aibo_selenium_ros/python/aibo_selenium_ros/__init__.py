d3c8da075469e513cb06bc9efed04d30f6aac43e
Koki Shinjo <sktometometo@gmail.com>
2023-05-08 17:13:22 +0900

    [aibo_selenium_ros] add user agent

--- jsk_aibo_robot/aibo_selenium_ros/python/aibo_selenium_ros/__init__.py
+++ jsk_aibo_robot/aibo_selenium_ros/python/aibo_selenium_ros/__init__.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import logging
+import random
 import time
 
 import selenium
@@ -12,6 +13,13 @@ from selenium.webdriver.common.by import By
 logger = logging.getLogger(__name__)
 
 
+ 
+user_agent = [
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    ]
+ 
 class AIBOBrowserInterface(object):
 
   def __init__(self,
@@ -27,6 +35,7 @@ class AIBOBrowserInterface(object):
 
     options = Options()
     options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--user-agent=' + user_agent[random.randrange(0, len(user_agent), 1)])
     if headless:
       options.add_argument('--headless')
 
