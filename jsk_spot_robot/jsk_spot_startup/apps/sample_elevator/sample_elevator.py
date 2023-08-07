0c82ff74bcca72f8af8a161a7a93a7b63480db4c
Koki Shinjo <sktometometo@gmail.com>
2023-08-08 03:41:15 +0900

    [jsk_spot_startup] fix app

--- jsk_spot_robot/jsk_spot_startup/apps/sample_elevator/sample_elevator.py
+++ jsk_spot_robot/jsk_spot_startup/apps/sample_elevator/sample_elevator.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     client.power_on()
     client.undock()
 
-    behavior_client.send_goal_and_wait(NavigationGoal(target_node_id="eng2_7FElevator"))
+    behavior_client.send_goal_and_wait(NavigationGoal(target_node_id="eng2_3FElevator"))
     result = behavior_client.get_result()
     rospy.loginfo("Result forward: {} {}".format(result.success, result.message))
 
