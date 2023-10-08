6575f197894a4b80c82640870afd51bad62ef026
Koki Shinjo <sktometometo@gmail.com>
2023-10-08 12:41:56 +0900

    update

--- jsk_spot_robot/jsk_spot_startup/scripts/update-network-connection.sh
+++ jsk_spot_robot/jsk_spot_startup/scripts/update-network-connection.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 IF_ETH=enxa0cec875af37
-IF_LTE=enxf8b7975c750a
 IF_WIFI=wlxc006c31b1a80
+IF_LTE=enxf8b7975c750a
 WIFI_PROFILE=sanshiro
+CURRENT_CONNECTION=
 
 function existIF() {
     interface_name=$1
@@ -20,21 +21,21 @@ function existIF() {
 
 function connectETH() {
     sudo ifmetric $IF_ETH 100
-    sudo ifmetric $IF_LTE 101
     sudo ifmetric $IF_WIFI 101
+    sudo ifmetric $IF_LTE 101
 }
 
 function restartWIFI() {
     sudo nmcli connection down $WIFI_PROFILE
-    sudo ip l set $IF_WIFI down
-    sudo ip l set $IF_WIFI up
+    #sudo ip l set $IF_WIFI down
+    #sudo ip l set $IF_WIFI up
     sudo nmcli connection up $WIFI_PROFILE
 }
 
 function connectWIFI() {
     sudo ifmetric $IF_WIFI 100
-    sudo ifmetric $IF_LTE 101
     sudo ifmetric $IF_ETH 102
+    sudo ifmetric $IF_LTE 101
 }
 
 function connectLTE() {
@@ -43,25 +44,68 @@ function connectLTE() {
     sudo ifmetric $IF_ETH 102
 }
 
+function updateConnectionToETH() {
+    if [ $(existIF $IF_ETH) = 0 ]; then
+        ping -c 1 -W 1 1.1.1.1 -I $IF_ETH > /dev/null 2>&1
+        if [ $? = 0 ]; then
+            echo "Ethernet is online. switched to ethernet."
+            CURRENT_CONNECTION="ETH"
+            connectETH
+        else
+            echo "Ethernet is offline.: $?"
+        fi
+    else
+        echo "No ethernet device is found."
+    fi
+}
+
+function updateConnectionToWiFi() {
+    if [ $(existIF $IF_WIFI) = 0 ]; then
+        ping -c 1 -W 1 1.1.1.1 -I $IF_WIFI > /dev/null 2>&1
+        if [ $? = 0 ]; then
+            echo "Wifi is online. switched to wifi."
+            CURRENT_CONNECTION="WiFi"
+            connectWIFI
+        else
+            echo "Wifi is offline.: $?"
+        fi
+    else
+        echo "No wifi device is found."
+    fi
+}
+
+function updateConnectionToLTE() {
+    if [ $(existIF $IF_LTE) = 0 ]; then
+        echo "Connection type switched to lte"
+        CURRENT_CONNECTION="LTE"
+        connectLTE
+    else
+        echo "No lte device is found"
+    fi
+
+    echo "No network device found."
+}
+
 function updateConnection() {
 
     echo "Connection updating."
 
     # Reconnect WIFI
     if [ $(existIF $IF_WIFI) = 0 ]; then
-        ping -c 1 -W 1 1.1.1.1 -I $IF_WIFI
+        ping -c 1 -W 1 133.11.216.240 -I $IF_WIFI > /dev/null 2>&1
         if [ $? = 0 ]; then
             echo "wifi connected"
         else
             echo "wifi not connected. trying to connect $WIFI_PROFILE"
             restartWIFI
+            updateConnectionToWiFi
         fi
     else
         echo "wifi device not found. skipped wifi reconnection."
     fi
 
     # Check internet connection
-    ping -c 1 -W 1 1.1.1.1
+    ping -c 1 -W 1 1.1.1.1 > /dev/null 2>&1
     if [ $? = 0 ]; then
         echo "Network connected"
         return 0
@@ -69,49 +113,20 @@ function updateConnection() {
         echo "Network disconnected. Trying to reconnect."
     fi
 
-    # connect with Ethernet if available
-    if [ $(existIF $IF_ETH) = 0 ]; then
-        ping -c 1 -W 1 1.1.1.1 -I $IF_ETH
-        if [ $? = 0 ]; then
-            echo "Ethernet is online. switched to ethernet."
-            connectETH
-            return 0
-        else
-            echo "Ethernet is offline."
-        fi
-    else
-        echo "No ethernet device is found."
-    fi
+    # First, try ethernet to connect with Ethernet if available
+    updateConnectionToETH
 
-    # connect with Wi-Fi if available
-    if [ $(existIF $IF_WIFI) = 0 ]; then
-        ping -c 1 -W 1 1.1.1.1 -I $IF_WIFI
-        if [ $? = 0 ]; then
-            echo "Wifi is online. switched to wifi."
-            connectWIFI
-            return 0
-        else
-            echo "Wifi is offline."
-        fi
-    else
-        echo "No wifi device is found."
-    fi
+    # Second, try wifi to connect with Wi-Fi if available
+    updateConnectionToWiFi
 
     # connect with LTE if available
-    if [ $(existIF $IF_LTE) = 0 ]; then
-        echo "Connection type switched to lte"
-        connectLTE
-        return 1
-    else
-        echo "No lte device is found"
-    fi
-
-    echo "No network device found."
-    return 1
+    updateConnectionToLTE
 }
 
+connectETH
+CURRENT_CONNECTION="ETH"
 while :
 do
-    sleep 1
+    sleep 5
     updateConnection
 done
